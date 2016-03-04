491240e87244cd2b73cd02427d609b506c60909f
unknown <jerry.guan@STKWX32552.softtek.com>
2016-03-04 17:51:30 +0800

    no message

---
+++ app/src/main/java/com/softtek/lai/module/login/presenter/IPasswordPresenter.java
@@ -0,0 +1,10 @@
+package com.softtek.lai.module.login.presenter;
+
+/**
+ * Created by jerry.guan on 3/4/2016.
+ */
+public interface IPasswordPresenter {
+
+    //重置密码
+    void resetPassword(String password1,String password2);
+}
