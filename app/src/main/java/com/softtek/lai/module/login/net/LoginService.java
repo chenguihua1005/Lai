19e83828fc51d942c7865a0f355349ff04863e2c
Jerry Guan <jerry.guan@softtek.com>
2016-03-07 09:14:44 +0800

    登录模块修改

--- app/src/main/java/com/softtek/lai/module/login/net/LoginService.java
+++ app/src/main/java/com/softtek/lai/module/login/net/LoginService.java
@@ -10,11 +10,11 @@ import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
 import retrofit.http.POST;
-import retrofit.http.Path;
 import retrofit.http.Query;
 
 /**
  * Created by jerry.guan on 3/3/2016.
+ *
  */
 public interface LoginService {
 
