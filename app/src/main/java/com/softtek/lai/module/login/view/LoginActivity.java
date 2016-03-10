fb684d6d320ab156a7f6debe822cf19f75bf5e98
lareina <15961374509@sina.cn>
2016-03-10 16:46:34 +0800

    no message

--- app/src/main/java/com/softtek/lai/module/login/view/LoginActivity.java
+++ app/src/main/java/com/softtek/lai/module/login/view/LoginActivity.java
@@ -1,11 +1,9 @@
 package com.softtek.lai.module.login.view;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
