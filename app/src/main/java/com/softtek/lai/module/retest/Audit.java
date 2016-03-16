a268ce79c07afcb94c86e668e10bf18a07f7032e
lareina <15961374509@sina.cn>
2016-03-16 20:09:44 +0800

    <imp lareina>功能模块界面

--- app/src/main/java/com/softtek/lai/module/retest/Audit.java
+++ app/src/main/java/com/softtek/lai/module/retest/Audit.java
@@ -13,8 +13,8 @@ import com.softtek.lai.utils.DisplayUtil;
 import butterknife.InjectView;
 import zilla.libcore.ui.InjectLayout;
 
-@InjectLayout(R.layout.activity_write)
-public class Write extends BaseActivity {
+@InjectLayout(R.layout.activity_audit)
+public class Audit extends BaseActivity {
     //标题栏
     @InjectView(R.id.tv_title)
     TextView title;
@@ -22,10 +22,10 @@ public class Write extends BaseActivity {
     TextView bar_left;
     @InjectView(R.id.tv_right)
     TextView bar_rignt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -37,10 +37,9 @@ public class Write extends BaseActivity {
     protected void initDatas() {
         bar_left.setBackgroundResource(R.drawable.back_h);
         bar_left.setLayoutParams(new Toolbar.LayoutParams(DisplayUtil.dip2px(this,15),DisplayUtil.dip2px(this,20)));
-        title.setText("复测录入");
-        bar_rignt.setText("保存");
+        title.setText("复测审核");
+        bar_rignt.setText("确认");
         bar_rignt.setTextColor(Color.BLACK);
-
 
     }
 }
