5874d868b852b2b57f7c3ce41f206855bb437493
lareina <15961374509@sina.cn>
2016-03-16 11:04:19 +0800

    <imp lareina>添加复测模块文件

---
+++ app/src/main/java/com/softtek/lai/module/retest/Write.java
@@ -0,0 +1,26 @@
+package com.softtek.lai.module.retest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+
+public class Write extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+
+    @Override
+    protected void initViews() {
+        
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
