2e0ec974b0a4ae73c3fea1ea936fff5f7ccb987c
guan <jerry.guan@softtek.com>
2016-03-18 16:44:55 +0800

    <imp jerry> 班级主页

---
+++ app/src/main/java/com/softtek/lai/module/grade/view/GradeHomeActivity.java
@@ -0,0 +1,29 @@
+package com.softtek.lai.module.grade.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+
+import zilla.libcore.ui.InjectLayout;
+
+@InjectLayout(R.layout.activity_grade_home)
+public class GradeHomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
