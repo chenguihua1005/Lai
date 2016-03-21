4ef8ec9ef5953febb3132554b7d082067f9b6336
guan <jerry.guan@softtek.com>
2016-03-21 16:15:05 +0800

    <imp jerry> 更新文件

--- app/src/main/java/com/softtek/lai/module/grade/net/GradeService.java
+++ app/src/main/java/com/softtek/lai/module/grade/net/GradeService.java
@@ -1,9 +1,21 @@
 package com.softtek.lai.module.grade.net;
 
+import com.softtek.lai.common.ResponseData;
+import com.softtek.lai.module.grade.model.Grade;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Query;
+
 /**
  * Created by jerry.guan on 3/21/2016.
  */
 public interface GradeService {
 
-
+    //获取班级主页信息
+    @GET("/HerbrClass/GetClassDetail")
+    void getGradeInfo(@Header("token")String token,
+                      @Query("ClassId")String classId,
+                      Callback<ResponseData<Grade>> gradeInfo);
 }
