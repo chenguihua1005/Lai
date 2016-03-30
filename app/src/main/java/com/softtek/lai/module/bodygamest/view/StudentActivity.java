034e0b3ec2499bbf8884814ffd6686cfc8e47e05
lareina <15961374509@sina.cn>
2016-03-30 13:19:55 +0800

    no message

--- app/src/main/java/com/softtek/lai/module/bodygamest/view/StudentActivity.java
+++ app/src/main/java/com/softtek/lai/module/bodygamest/view/StudentActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 
 import com.softtek.lai.R;
 import com.softtek.lai.common.BaseActivity;
+import com.softtek.lai.module.bodygame.model.FuceNum;
+import com.softtek.lai.module.bodygame.presenter.ITiGuanSai;
 
 import zilla.libcore.ui.InjectLayout;
 
 @InjectLayout(R.layout.activity_student)
 public class StudentActivity extends BaseActivity {
-
+    private ITiGuanSai tiGuanSai;
+    private FuceNum fuceNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
