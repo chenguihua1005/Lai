4c8dad1c0ccbc1f4980596c506d1c8237ae26ff7
jarvis <jarvis.liu@softtek.com>
2016-04-01 17:02:47 +0800

    学员版荣誉榜 逻辑接口

---
+++ app/src/main/java/com/softtek/lai/module/bodygamest/net/StudentService.java
@@ -0,0 +1,24 @@
+package com.softtek.lai.module.bodygamest.net;
+
+import com.softtek.lai.common.ResponseData;
+import com.softtek.lai.module.bodygamest.model.DownPhoto;
+import com.softtek.lai.module.bodygamest.model.StudentHonorInfo;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Query;
+
+/**
+ * Created by lareina.qiao on 3/31/2016.
+ */
+public interface StudentService {
+    //获取图片列表接口
+    @GET("/Index/GetStudentHonor")
+    void getStudentHonor(
+            @Header("token") String token,
+            Callback<ResponseData<List<StudentHonorInfo>>> callback
+    );
+}
---
+++ app/src/main/java/com/softtek/lai/module/bodygamest/view/StudentScoreActivity.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2010-2016 Softtek Information Systems (Wuxi) Co.Ltd.
+ * Date:2016-03-31
+ */
+
+package com.softtek.lai.module.bodygamest.view;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.mobsandgeeks.saripaar.Rule;
+import com.mobsandgeeks.saripaar.Validator;
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+import com.softtek.lai.common.BaseFragment;
+import com.softtek.lai.contants.Constants;
+import com.softtek.lai.module.counselor.adapter.SimpleFragmentPagerAdapter;
+import com.softtek.lai.module.counselor.presenter.AssistantImpl;
+import com.softtek.lai.module.counselor.presenter.IAssistantPresenter;
+import com.softtek.lai.module.counselor.view.AssistantApplyFragment;
+import com.softtek.lai.module.counselor.view.AssistantListFragment;
+import com.softtek.lai.utils.ACache;
+import com.softtek.lai.utils.SoftInputUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.InjectView;
+import zilla.libcore.lifecircle.LifeCircleInject;
+import zilla.libcore.lifecircle.validate.ValidateLife;
+import zilla.libcore.ui.InjectLayout;
+
+/**
+ * Created by jarvis.liu on 3/22/2016.
+ * 助教管理页面
+ */
+@InjectLayout(R.layout.activity_assistant)
+public class StudentScoreActivity extends BaseActivity implements View.OnClickListener, Validator.ValidationListener, BaseFragment.OnFragmentInteractionListener {
+
+    @LifeCircleInject
+    ValidateLife validateLife;
+
+
+    @InjectView(R.id.tv_left)
+    TextView tv_left;
+
+    @InjectView(R.id.tv_right)
+    TextView tv_right;
+
+    @InjectView(R.id.tv_title)
+    TextView tv_title;
+
+    @InjectView(R.id.list_student_core)
+    ListView list_student_core;
+
+
+
+    private IAssistantPresenter assistantPresenter;
+    private ACache aCache;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        tv_left.setOnClickListener(this);
+        tv_right.setOnClickListener(this);
+    }
+
+    @Override
+    protected void initViews() {
+        tv_left.setBackgroundResource(R.drawable.back);
+        //tv_left.setLayoutParams(new Toolbar.LayoutParams(DisplayUtil.dip2px(this,15),DisplayUtil.dip2px(this,30)));
+        tv_title.setText("成绩单");
+        tv_right.setText("分享");
+
+    }
+
+    @Override
+    protected void initDatas() {
+        assistantPresenter = new AssistantImpl(this);
+        aCache = ACache.get(this, Constants.USER_ACACHE_DATA_DIR);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        SoftInputUtil.hidden(this);
+        switch (v.getId()) {
+            case R.id.tv_left:
+                finish();
+                break;
+
+            case R.id.tv_right:
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+
+    }
+
+    @Override
+    public void onValidationFailed(View failedView, Rule<?> failedRule) {
+        validateLife.onValidationFailed(failedView, failedRule);
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+}
