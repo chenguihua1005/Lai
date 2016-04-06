7cdd4dada4e78f5616ff9306040a6c972e55b8ba
guan <jerry.guan@softtek.com>
2016-04-06 18:15:25 +0800

    jerry

--- app/src/main/java/com/softtek/lai/module/home/view/HomeFragment.java
+++ app/src/main/java/com/softtek/lai/module/home/view/HomeFragment.java
@@ -206,14 +206,14 @@ public class HomeFragment extends BaseFragment implements AppBarLayout.OnOffsetC
         }
         switch (position) {
             case 0:
-                startActivity(new Intent(getContext(), CounselorActivity.class));
+                //startActivity(new Intent(getContext(), CounselorActivity.class));
                 break;
             case 1:
-                startActivity(new Intent(getContext(), StudentActivity.class));
+               // startActivity(new Intent(getContext(), StudentActivity.class));
                 break;
             case 2:
-                retestPre=new RetestclassImp();
-                retestPre.doPostClient("client_credentials","shhcieurjfn734js","qieow8572jkcv");
+                //retestPre=new RetestclassImp();
+                //retestPre.doPostClient("client_credentials","shhcieurjfn734js","qieow8572jkcv");
                 break;
             case 3:
                 break;
