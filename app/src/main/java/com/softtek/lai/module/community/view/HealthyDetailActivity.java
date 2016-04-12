3581f12f28453a6b3417953e2017aeff0a4582bd
guanguoxiang <abcgg8@qq.com>
2016-04-12 18:26:58 +0800

    jerry

---
+++ app/src/main/java/com/softtek/lai/module/community/view/HealthyDetailActivity.java
@@ -0,0 +1,21 @@
+package com.softtek.lai.module.community.view;
+
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+
+import zilla.libcore.ui.InjectLayout;
+
+@InjectLayout(R.layout.activity_healthy_detail)
+public class HealthyDetailActivity extends BaseActivity {
+
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
