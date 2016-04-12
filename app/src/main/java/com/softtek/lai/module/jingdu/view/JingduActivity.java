ad193221f49bb59b353675ae247ae6c5fa4316e9
Julie.zhu <shen.abc@foxmail,com>
2016-04-12 10:04:52 +0800

    no message

--- app/src/main/java/com/softtek/lai/module/jingdu/view/JingduActivity.java
+++ app/src/main/java/com/softtek/lai/module/jingdu/view/JingduActivity.java
@@ -5,6 +5,7 @@
 
 package com.softtek.lai.module.jingdu.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +20,16 @@ import com.softtek.lai.module.jingdu.model.RankModel;
 import com.softtek.lai.module.jingdu.model.Table1Model;
 import com.softtek.lai.module.jingdu.presenter.GetProinfoImpl;
 import com.softtek.lai.module.jingdu.presenter.IGetProinfopresenter;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.media.SinaShareContent;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.sso.SinaSsoHandler;
+import com.umeng.socialize.sso.UMSsoHandler;
+import com.umeng.socialize.weixin.controller.UMWXHandler;
+import com.umeng.socialize.weixin.media.CircleShareContent;
+import com.umeng.socialize.weixin.media.WeiXinShareContent;
 
 
 import org.greenrobot.eventbus.EventBus;
@@ -59,6 +70,9 @@ public class JingduActivity extends BaseActivity implements View.OnClickListener
     String f = "";
     String g = "";
     String text;
+
+    // 首先在您的Activity中添加如下成员变量
+    final UMSocialService Controller = UMServiceFactory.getUMSocialService("com.umeng.share");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,7 +108,7 @@ public class JingduActivity extends BaseActivity implements View.OnClickListener
         a=rank.getTable().get(0).getTotalClass();
         b=rank.getTable().get(0).getTotalMember();
         c=rank.getTable().get(0).getTotalWeight();
-        tv_wz.setText( "本月新开班级"+a+"个,新增学员"+b+"名,累计减重"+c+"斤,其中1月班累计减重"+d+"斤,2月班本月累计减重"+e+"斤,3月班本月累计减重"+f+"斤, 相当于"+g+"头大象.");
+        tv_wz.setText( "本月新开班级"+a+"个,新增学员"+b+"名,累计减重"+c+"斤,其中1月班累计减重"+d+"斤,2月班本月累计减重"+e+"xx"+"斤,3月班本月累计减重"+f+"xx"+"斤, 相当于"+g+"xx"+"头大象.");
 
         table1ModelList=rank.getTable1();
         rankAdapter.updateData(table1ModelList);
@@ -108,8 +122,63 @@ public class JingduActivity extends BaseActivity implements View.OnClickListener
                 break;
             //分享功能逻辑
             case R.id.tv_right:
+//                Controller.setShareContent("友盟社会化组件（SDK）让移动应用快速整合社交分享功能，http://www.umeng.com/social");
+//                //   mController.setShareMedia(new UMImage(JingduActivity.this,"http://www.umeng.com/images/pic/banner_module_social.png"));
+//                // 设置分享图片，参数2为本地图片的资源引用
+//                Controller.setShareMedia(new UMImage(JingduActivity.this,R.mipmap.ic_launcher));
 
+                Controller.getConfig().setPlatforms(SHARE_MEDIA.WEIXIN,SHARE_MEDIA.WEIXIN_CIRCLE,SHARE_MEDIA.SINA);
+                // 设置分享内容
+                setShareContent();
+
+                // 添加新浪微博平台
+                Controller.getConfig().setSsoHandler(new SinaSsoHandler());
+                String appId = "wx46b7f1bdb5d0990a";
+                String appSecret ="e9a91be6ee2170767c447c233f8206fc";
+                // 添加微信平台
+                UMWXHandler wxHandler = new UMWXHandler(this, appId, appSecret);
+                wxHandler.addToSocialSDK();
+                // 添加微信朋友圈
+                UMWXHandler wxCircleHandler = new UMWXHandler(this, appId, appSecret);
+                wxCircleHandler.setToCircle(true);
+                wxCircleHandler.addToSocialSDK();
+
+                Controller.openShare(JingduActivity.this, false);
                 break;
+        }
+    }
+
+    private void setShareContent() {
+        WeiXinShareContent weixinContent = new WeiXinShareContent();
+        weixinContent.setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能，微信 www.qq.com");
+        weixinContent.setTitle("友盟社会化分享组件-微信");
+        weixinContent.setTargetUrl("www.baidu.com");
+        weixinContent.setShareMedia(new UMImage(this, R.drawable.umeng_socialize_wechat));
+        Controller.setShareMedia(weixinContent);
+
+        CircleShareContent circleMedia = new CircleShareContent();
+        circleMedia.setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能，微信");
+        circleMedia.setTitle("友盟社会化分享组件-微信");
+        circleMedia.setShareMedia(new UMImage(this, R.drawable.umeng_socialize_wxcircle));
+        // circleMedia.setShareMedia(uMusic);
+        // circleMedia.setShareMedia(video);
+        circleMedia.setTargetUrl("http://www.umeng.com/social");
+        Controller.setShareMedia(circleMedia);
+
+        SinaShareContent sinaContent = new SinaShareContent();
+        sinaContent.setShareContent("http://dev.umeng.com/social/android/android-update#9");
+        sinaContent.setTargetUrl("www.baidu.com");
+        sinaContent.setShareMedia(new UMImage(this, R.drawable.umeng_socialize_sina_on));
+        Controller.setShareMedia(sinaContent);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        /**使用SSO授权必须添加如下代码 */
+        UMSsoHandler ssoHandler = Controller.getConfig().getSsoHandler(requestCode) ;
+        if(ssoHandler != null){
+            ssoHandler.authorizeCallBack(requestCode, resultCode, data);
         }
     }
 }
