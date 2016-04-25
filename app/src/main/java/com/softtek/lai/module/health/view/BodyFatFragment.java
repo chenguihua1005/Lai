74fd16637fdcad97b015b070f1c9d2e3d0bfd29e
良人用情未深 <15961374509@sina.cn>
2016-04-26 01:23:43 +0800

    lareina

--- app/src/main/java/com/softtek/lai/module/health/view/BodyFatFragment.java
+++ app/src/main/java/com/softtek/lai/module/health/view/BodyFatFragment.java
@@ -260,69 +260,199 @@ public class BodyFatFragment extends BaseFragment implements RadioGroup.OnChecke
         {
 
             case R.id.bt_left: {
-
-                if (state!=true)
+                switch (flag)
                 {
-                    n=n+7;
+                    case 0:
+                        if (state!=true)
+                        {
+                            n=n+7;
+                        }
+                        state=true;
+                        days.clear();
+                        dates.clear();
+                        String nowdate7 = getPeriodDate(type, n) + "";
+                        String nowdate6 = getPeriodDate(type, n + 1) + "";
+                        String nowdate5 = getPeriodDate(type, n + 2) + "";
+                        String nowdate4 = getPeriodDate(type, n + 3) + "";
+                        String nowdate3 = getPeriodDate(type, n + 4) + "";
+                        String nowdate2 = getPeriodDate(type, n + 5) + "";
+                        String nowdate1 = getPeriodDate(type, n + 6) + "";
+                        days.add(formdate(nowdate1));
+                        days.add(formdate(nowdate2));
+                        days.add(formdate(nowdate3));
+                        days.add(formdate(nowdate4));
+                        days.add(formdate(nowdate5));
+                        days.add(formdate(nowdate6));
+                        days.add(formdate(nowdate7));
+                        healthRecordManager.doGetHealthPysicalRecords(getDateform(nowdate7)+" "+datetime[1],getDateform(nowdate1)+" "+datetime[1],1);
+                        n = n + 7;
+                        bt_right.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        if (state!=true)
+                        {
+                            n=n+4;
+                        }
+                        state=true;
+                        dates.clear();
+                        days.clear();
+                        flag=1;
+                        type='6';
+                        String monthdate4=getPeriodDate(type,7*n)+"";
+                        String monthdate3=getPeriodDate(type,7*(n+1))+"";
+                        String monthdate2=getPeriodDate(type,7*(n+2))+"";
+                        String monthdate1=getPeriodDate(type,7*(n+3))+"";
+                        days.add(formdate(monthdate1));
+                        days.add(formdate(monthdate2));
+                        days.add(formdate(monthdate3));
+                        days.add(formdate(monthdate4));
+                        healthRecordManager.doGetHealthPysicalRecords(getDateform(monthdate4)+" "+datetime[1],getDateform(monthdate1)+" "+datetime[1],2);
+                        n=n+4;
+                        bt_right.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        if (state!=true)
+                        {
+                            n=n+4;
+                        }
+                        state=true;
+                        dates.clear();
+                        days.clear();
+                        flag=1;
+                        type='6';
+                        String quarterdate4=getPeriodDate(type,21*n)+"";
+                        String quarterdate3=getPeriodDate(type,21*(n+1))+"";
+                        String quarterdate2=getPeriodDate(type,21*(n+2))+"";
+                        String quarterdate1=getPeriodDate(type,21*(n+3))+"";
+                        days.add(formdate(quarterdate1));
+                        days.add(formdate(quarterdate2));
+                        days.add(formdate(quarterdate3));
+                        days.add(formdate(quarterdate4));
+                        healthRecordManager.doGetHealthPysicalRecords(getDateform(quarterdate4)+" "+datetime[1],getDateform(quarterdate1)+" "+datetime[1],3);
+                        n=n+4;
+                        bt_right.setVisibility(View.VISIBLE);
+                        break;
+                    case 3:
+                        if (state!=true)
+                        {
+                            n=n+4;
+                        }
+                        dates.clear();
+                        days.clear();
+                        state=true;
+                        bt_right.setVisibility(View.GONE);
+                        type='7';
+                        n=7;
+                        chartUtil.addData(dates,4,days);
+                        String yeardate4=getPeriodDate(type,2*n)+"";
+                        String yeardate3=getPeriodDate(type,2*(n+1))+"";
+                        String yeardate2=getPeriodDate(type,2*(n+2))+"";
+                        String yeardate1=getPeriodDate(type,2*(n+3))+"";
+                        String yeardate0=getPeriodDate(type,2*(n+4))+"";
+                        days.add(formyeardate(yeardate1));
+                        days.add(formyeardate(yeardate2));
+                        days.add(formyeardate(yeardate3));
+                        days.add(formyeardate(yeardate4));
+                        healthRecordManager.doGetHealthPysicalRecords(getDateform(yeardate4)+" "+datetime[1],getDateform(yeardate0)+" "+datetime[1],4);
+                        break;
                 }
-                state=true;
-                days.clear();
-                dates.clear();
-                String nowdate7 = getPeriodDate(type, n) + "";
-                String nowdate6 = getPeriodDate(type, n + 1) + "";
-                String nowdate5 = getPeriodDate(type, n + 2) + "";
-                String nowdate4 = getPeriodDate(type, n + 3) + "";
-                String nowdate3 = getPeriodDate(type, n + 4) + "";
-                String nowdate2 = getPeriodDate(type, n + 5) + "";
-                String nowdate1 = getPeriodDate(type, n + 6) + "";
-                days.add(formdate(nowdate1));
-                days.add(formdate(nowdate2));
-                days.add(formdate(nowdate3));
-                days.add(formdate(nowdate4));
-                days.add(formdate(nowdate5));
-                days.add(formdate(nowdate6));
-                days.add(formdate(nowdate7));
-                healthRecordManager.doGetHealthPysicalRecords(getDateform(nowdate7)+" "+datetime[1],getDateform(nowdate1)+" "+datetime[1],1);
-                n = n + 7;
-                bt_right.setVisibility(View.VISIBLE);
+
+
 
             }
                 break;
             case R.id.bt_right:
-                days.clear();
+                switch (flag)
+                {
+                    case 0:
+                        if (state!=false) {
+                            n = n - 14;
+                        }
+                        else {
+                            n=n-7;
+                        }
+                        dates.clear();
+                        days.clear();
+                        String nowdate7 = getPeriodDate(type, n) + "";
+                        String nowdate6 = getPeriodDate(type, n + 1) + "";
+                        String nowdate5 = getPeriodDate(type, n + 2) + "";
+                        String nowdate4 = getPeriodDate(type, n + 3) + "";
+                        String nowdate3 = getPeriodDate(type, n + 4) + "";
+                        String nowdate2 = getPeriodDate(type, n + 5) + "";
+                        String nowdate1 = getPeriodDate(type, n + 6) + "";
+                        days.add(formdate(nowdate1));
+                        days.add(formdate(nowdate2));
+                        days.add(formdate(nowdate3));
+                        days.add(formdate(nowdate4));
+                        days.add(formdate(nowdate5));
+                        days.add(formdate(nowdate6));
+                        days.add(formdate(nowdate7));
+                        healthRecordManager.doGetHealthPysicalRecords(getDateform(nowdate7)+" "+datetime[1],getDateform(nowdate1)+" "+datetime[1],1);
+                        state=false;
+                        if (nowdate7.equals(getPeriodDate(type,0)+""))
+                            bt_right.setVisibility(View.GONE);
+                        break;
+                    case 1:
+                        if (state!=false) {
+                            n = n - 8;
+                        }
+                        else {
+                            n=n-4;
 
-                if (state!=false) {
-                    n = n - 14;
+                        }
+                        dates.clear();
+                        days.clear();
+                        type='6';
+                        String monthdate4=getPeriodDate(type,7*n)+"";
+                        String monthdate3=getPeriodDate(type,7*(n+1))+"";
+                        String monthdate2=getPeriodDate(type,7*(n+2))+"";
+                        String monthdate1=getPeriodDate(type,7*(n+3))+"";
+                        days.add(formdate(monthdate1));
+                        days.add(formdate(monthdate2));
+                        days.add(formdate(monthdate3));
+                        days.add(formdate(monthdate4));
+                        healthRecordManager.doGetHealthPysicalRecords(getDateform(monthdate4)+" "+datetime[1],getDateform(monthdate1)+" "+datetime[1],2);
+                        state=false;
+                        if (monthdate4.equals(getPeriodDate(type,0)+""))
+                            bt_right.setVisibility(View.GONE);
+                        break;
+                    case 2:
+                        if (state!=false) {
+                            n = n - 8;
+                        }
+                        else {
+                            n=n-4;
+
+                        }
+                        dates.clear();
+                        days.clear();
+                        type='6';
+                        String quarterdate4=getPeriodDate(type,21*n)+"";
+                        String quarterdate3=getPeriodDate(type,21*(n+1))+"";
+                        String quarterdate2=getPeriodDate(type,21*(n+2))+"";
+                        String quarterdate1=getPeriodDate(type,21*(n+3))+"";
+                        days.add(formdate(quarterdate1));
+                        days.add(formdate(quarterdate2));
+                        days.add(formdate(quarterdate3));
+                        days.add(formdate(quarterdate4));
+                        healthRecordManager.doGetHealthPysicalRecords(getDateform(quarterdate4)+" "+datetime[1],getDateform(quarterdate1)+" "+datetime[1],3);
+                        bt_right.setVisibility(View.VISIBLE);
+                        state=false;
+                        if (quarterdate4.equals(getPeriodDate(type,0)+""))
+                            bt_right.setVisibility(View.GONE);
+                        break;
+                    case 3:
+                        break;
                 }
-                else {
-                    n=n-7;
-                }
-                dates.clear();
-                String nowdate7 = getPeriodDate(type, n) + "";
-                String nowdate6 = getPeriodDate(type, n + 1) + "";
-                String nowdate5 = getPeriodDate(type, n + 2) + "";
-                String nowdate4 = getPeriodDate(type, n + 3) + "";
-                String nowdate3 = getPeriodDate(type, n + 4) + "";
-                String nowdate2 = getPeriodDate(type, n + 5) + "";
-                String nowdate1 = getPeriodDate(type, n + 6) + "";
-                days.add(formdate(nowdate1));
-                days.add(formdate(nowdate2));
-                days.add(formdate(nowdate3));
-                days.add(formdate(nowdate4));
-                days.add(formdate(nowdate5));
-                days.add(formdate(nowdate6));
-                days.add(formdate(nowdate7));
-                healthRecordManager.doGetHealthPysicalRecords(getDateform(nowdate7)+" "+datetime[1],getDateform(nowdate1)+" "+datetime[1],1);
-                days.clear();
-                state=false;
-                if (nowdate7.equals(getPeriodDate(type,0)+""))
-                    bt_right.setVisibility(View.GONE);
-//                n = n - 7;
-//                sta=false;
+
+
                 break;
             case R.id.week:
                 flag=0;
                 type='6';
+                n=7;
+                state=true;
+                bt_right.setVisibility(View.GONE);
                 dates.clear();
                 days.clear();
                 String weekdate7=getPeriodDate(type,0)+"";
@@ -343,11 +473,13 @@ public class BodyFatFragment extends BaseFragment implements RadioGroup.OnChecke
 
                 break;
             case R.id.month:
-                ceshi.get(0);
                 dates.clear();
                 days.clear();
                 flag=1;
+                state=true;
+                bt_right.setVisibility(View.GONE);
                 type='6';
+                n=4;
                 String monthdate4=getPeriodDate(type,0)+"";
                 String monthdate3=getPeriodDate(type,7)+"";
                 String monthdate2=getPeriodDate(type,14)+"";
@@ -356,14 +488,16 @@ public class BodyFatFragment extends BaseFragment implements RadioGroup.OnChecke
                 days.add(formdate(monthdate2));
                 days.add(formdate(monthdate3));
                 days.add(formdate(monthdate4));
-                healthRecordManager.doGetHealthPysicalRecords(date,getDateform(monthdate1)+" "+datetime[1],2);
-
+                healthRecordManager.doGetHealthPysicalRecords(getDateform(monthdate4)+" "+datetime[1],getDateform(monthdate1)+" "+datetime[1],2);
                 break;
             case R.id.quarter:
                 dates.clear();
                 days.clear();
-                flag=1;
+                flag=2;
+                state=true;
+                bt_right.setVisibility(View.GONE);
                 type='6';
+                n=4;
                 String quarterdate4=getPeriodDate(type,0)+"";
                 String quarterdate3=getPeriodDate(type,21)+"";
                 String quarterdate2=getPeriodDate(type,21*2)+"";
@@ -372,13 +506,16 @@ public class BodyFatFragment extends BaseFragment implements RadioGroup.OnChecke
                 days.add(formdate(quarterdate2));
                 days.add(formdate(quarterdate3));
                 days.add(formdate(quarterdate4));
-                healthRecordManager.doGetHealthPysicalRecords(date,getDateform(quarterdate1)+" "+datetime[1],3);
+                healthRecordManager.doGetHealthPysicalRecords(getDateform(quarterdate4)+" "+datetime[1],getDateform(quarterdate1)+" "+datetime[1],3);
                 break;
             case R.id.year:
-                flag=1;
+                flag=3;
                 dates.clear();
                 days.clear();
+                state=true;
+                bt_right.setVisibility(View.GONE);
                 type='7';
+                n=7;
                 chartUtil.addData(dates,4,days);
                 String yeardate4=getPeriodDate(type,0)+"";
                 String yeardate3=getPeriodDate(type,2)+"";
@@ -389,7 +526,7 @@ public class BodyFatFragment extends BaseFragment implements RadioGroup.OnChecke
                 days.add(formyeardate(yeardate2));
                 days.add(formyeardate(yeardate3));
                 days.add(formyeardate(yeardate4));
-                healthRecordManager.doGetHealthPysicalRecords(date,getDateform(yeardate0)+" "+datetime[1],4);
+                healthRecordManager.doGetHealthPysicalRecords(getDateform(yeardate4)+" "+datetime[1],getDateform(yeardate0)+" "+datetime[1],4);
                 break;
         }
     }
