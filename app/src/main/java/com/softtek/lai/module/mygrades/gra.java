68a487891b104b39006d8249e4dda234283fd636
Julie.zhu <shen.abc@foxmail,com>
2016-05-03 18:11:13 +0800

    提交莱运动我的成绩

--- app/src/main/java/com/softtek/lai/module/mygrades/gra.java
+++
@@ -1,7 +0,0 @@
-package com.softtek.lai.module.mygrades;
-
-/**
- * Created by julie.zhu on 4/29/2016.
- */
-public class gra {
-}
---
+++ app/src/main/java/com/softtek/lai/module/mygrades/model/orderDataModel.java
@@ -0,0 +1,82 @@
+package com.softtek.lai.module.mygrades.model;
+
+/**
+ * Created by julie.zhu on 5/3/2016.
+ */
+public class orderDataModel {
+    private String AccountId;
+    private String Photo;
+    private String _order;
+    private String mobile;
+    private String stepCount;
+    private String userName;
+
+    public orderDataModel(String accountId, String photo, String _order, String mobile, String stepCount, String userName) {
+        AccountId = accountId;
+        Photo = photo;
+        this._order = _order;
+        this.mobile = mobile;
+        this.stepCount = stepCount;
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "orderDataModel{" +
+                "AccountId='" + AccountId + '\'' +
+                ", Photo='" + Photo + '\'' +
+                ", _order='" + _order + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", stepCount='" + stepCount + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+
+    public String getAccountId() {
+        return AccountId;
+    }
+
+    public void setAccountId(String accountId) {
+        AccountId = accountId;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String photo) {
+        Photo = photo;
+    }
+
+    public String get_order() {
+        return _order;
+    }
+
+    public void set_order(String _order) {
+        this._order = _order;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getStepCount() {
+        return stepCount;
+    }
+
+    public void setStepCount(String stepCount) {
+        this.stepCount = stepCount;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
