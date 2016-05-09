f08e5e859a99a1df42ee808c53ca14c3b668035b
guan <jerry.guan@softtek.com>
2016-05-09 17:23:52 +0800

    jerry

---
+++ app/src/main/java/com/softtek/lai/module/personalPK/view/CreatePKActivity.java
@@ -0,0 +1,24 @@
+package com.softtek.lai.module.personalPK.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+
+import zilla.libcore.ui.InjectLayout;
+
+@InjectLayout(R.layout.activity_create_pk)
+public class CreatePKActivity extends BaseActivity {
+
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
