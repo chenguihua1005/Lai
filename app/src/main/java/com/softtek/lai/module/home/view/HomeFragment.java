ecd2b58667db3bcda5d7879dfb050cf5432c3d3a
jarvis <jarvis.liu@softtek.com>
2016-05-10 10:44:52 +0800

    参赛邀请bug

--- app/src/main/java/com/softtek/lai/module/home/view/HomeFragment.java
+++ app/src/main/java/com/softtek/lai/module/home/view/HomeFragment.java
@@ -278,7 +278,7 @@ public class HomeFragment extends BaseFragment implements AppBarLayout.OnOffsetC
                     break;
                 case Constants.LAI_YUNDONG:
                     //startActivity(new Intent(getContext(), PKListActivity.class));
-                    new AlertDialog.Builder(getContext()).setMessage("功能开发中敬请期待").create().show();
+                     new AlertDialog.Builder(getContext()).setMessage("功能开发中敬请期待").create().show();
                     //sportGroupManager.isJoinRunGroup(UserInfoModel.getInstance().getUser().getUserid());
                     break;
                 case Constants.OFFICE:
