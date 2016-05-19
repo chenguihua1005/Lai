aa1a61eee24d018369713c44cdb34b0776569107
guanguoxiang <abcgg8@qq.com>
2016-05-20 00:17:48 +0800

    jerry

--- app/src/main/java/com/softtek/lai/common/BaseActivity.java
+++ app/src/main/java/com/softtek/lai/common/BaseActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.softtek.lai.LaiApplication;
 import com.softtek.lai.R;
