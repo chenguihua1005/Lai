d490064f0da9c9bbf1c2da22a130c32db462562c
guan <jerry.guan@softtek.com>
2016-06-28 10:28:24 +0800

    jerry 往期回顾

---
+++ app/src/main/java/com/softtek/lai/module/pastreview/presenter/PastReviewManager.java
@@ -0,0 +1,7 @@
+package com.softtek.lai.module.pastreview.presenter;
+
+/**
+ * Created by jerry.guan on 6/28/2016.
+ */
+public class PastReviewManager {
+}
---
+++ app/src/main/java/com/softtek/lai/module/pastreview/view/PcPastBaseDataActivity.java
@@ -0,0 +1,23 @@
+package com.softtek.lai.module.pastreview.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+
+import zilla.libcore.ui.InjectLayout;
+
+@InjectLayout(R.layout.activity_pc_past_base_data)
+public class PcPastBaseDataActivity extends BaseActivity {
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
