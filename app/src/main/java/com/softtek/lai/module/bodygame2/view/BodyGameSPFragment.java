4ce5740fc72ac9e4139df2ff51bfef21c3a4855d
guan <jerry.guan@softtek.com>
2016-07-11 17:49:49 +0800

    jerry

--- app/src/main/java/com/softtek/lai/module/bodygame2/view/BodyGameSPFragment.java
+++ app/src/main/java/com/softtek/lai/module/bodygame2/view/BodyGameSPFragment.java
@@ -1,17 +1,27 @@
 package com.softtek.lai.module.bodygame2.view;
 
+import android.widget.RelativeLayout;
+
 import com.github.snowdream.android.util.Log;
 import com.softtek.lai.R;
 import com.softtek.lai.common.LazyBaseFragment;
+import com.softtek.lai.utils.DisplayUtil;
 
+import butterknife.InjectView;
 import zilla.libcore.ui.InjectLayout;
 
 @InjectLayout(R.layout.fragment_bodygame_sp)
 public class BodyGameSPFragment extends LazyBaseFragment {
 
+    @InjectView(R.id.toolbar)
+    RelativeLayout relativeLayout;
+
     @Override
     protected void initViews() {
-
+        int status=DisplayUtil.getStatusHeight(getActivity());
+        RelativeLayout.LayoutParams params= (RelativeLayout.LayoutParams) relativeLayout.getLayoutParams();
+        params.topMargin=status;
+        relativeLayout.setLayoutParams(params);
     }
 
     @Override
