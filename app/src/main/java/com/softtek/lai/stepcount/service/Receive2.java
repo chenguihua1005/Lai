3f5b233f56e5ba2ae135d65812ee56b1822add84
guanguoxiang <abcgg8@qq.com>
2016-07-16 02:38:16 +0800

    jerry

---
+++ app/src/main/java/com/softtek/lai/stepcount/service/Receive2.java
@@ -0,0 +1,16 @@
+package com.softtek.lai.stepcount.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by John on 2016/7/16.
+ */
+public class Receive2 extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
