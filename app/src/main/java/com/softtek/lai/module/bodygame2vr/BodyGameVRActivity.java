10c5db6f804f05bc5c0fa6a11d5849ee061a000d
guan <jerry.guan@softtek.com>
2016-07-16 12:38:34 +0800

    jerry

---
+++ app/src/main/java/com/softtek/lai/module/bodygame2vr/BodyGameVRActivity.java
@@ -0,0 +1,24 @@
+package com.softtek.lai.module.bodygame2vr;
+
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+
+import zilla.libcore.ui.InjectLayout;
+
+/**
+ * 体管赛游客版
+ */
+@InjectLayout(R.layout.activity_body_game_vr)
+public class BodyGameVRActivity extends BaseActivity {
+
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
