b1b52eb678f38a5126d7dbf0fd6e469fa2e95d1f
jarvis.liu <jarvis.liu@softtek.com>
2016-07-25 16:43:10 +0800

    bug

--- app/src/main/java/com/softtek/lai/module/bodygame2/view/StudentHonorPCActivity.java
+++ app/src/main/java/com/softtek/lai/module/bodygame2/view/StudentHonorPCActivity.java
@@ -88,10 +88,10 @@ public class StudentHonorPCActivity extends BaseActivity implements View.OnClick
     protected void initViews() {
         int type=getIntent().getIntExtra("type",0);
         if (type==1) {
-            tv_title.setText("荣誉榜");
+            tv_title.setText("学员荣誉榜");
         }
         else {
-            tv_title.setText("我的荣誉榜");
+            tv_title.setText("荣誉榜");
         }
 
         ll_left.setOnClickListener(this);
