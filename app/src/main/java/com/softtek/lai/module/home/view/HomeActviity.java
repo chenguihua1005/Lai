ac558ae1937d24d501f3f1f9544e0c824e31a391
guan <jerry.guan@softtek.com>
2016-09-02 09:25:49 +0800

    jerry

--- app/src/main/java/com/softtek/lai/module/home/view/HomeActviity.java
+++ app/src/main/java/com/softtek/lai/module/home/view/HomeActviity.java
@@ -1,31 +1,20 @@
 package com.softtek.lai.module.home.view;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
-import android.view.KeyEvent;
 import android.view.View;
 
 import com.ggx.jerryguan.widget_lib.SimpleButton;
-import com.github.snowdream.android.util.Log;
 import com.softtek.lai.R;
 import com.softtek.lai.common.BaseActivity;
 import com.softtek.lai.common.BaseFragment;
 import com.softtek.lai.module.home.adapter.MainPageAdapter;
 
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.InjectView;
-import im.fir.sdk.FIR;
-import im.fir.sdk.VersionCheckCallback;
 import zilla.libcore.ui.InjectLayout;
 
 @InjectLayout(R.layout.activity_home_actviity)
@@ -57,7 +46,6 @@ public class HomeActviity extends BaseActivity implements View.OnClickListener, 
         btn_healthy.setOnClickListener(this);
         btn_healthy_record.setOnClickListener(this);
         btn_mine.setOnClickListener(this);
-        //checkUpdate();
         content.setOffscreenPageLimit(3);
 
     }
@@ -161,136 +149,6 @@ public class HomeActviity extends BaseActivity implements View.OnClickListener, 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
-    }
-
-    //**********************************************************
-
-    /**
-     * 获取网上软件版本号
-     * 检查本地版本号
-     **/
-
-    int version1;
-    int build;
-    String information;
-    String installUrl;
-
-    /**
-     * 获取版本号
-     *
-     * @return 当前应用的版本号
-     */
-    public void initVersionCode() {
-        try {
-            PackageManager manager = this.getPackageManager();
-            PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
-            version1 = info.versionCode;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void checkUpdate() {
-        initVersionCode();
-        FIR.checkForUpdateInFIR("b405d60358cdcb42b9c9d06f6e1d7918", new VersionCheckCallback() {
-            @Override
-            public void onSuccess(String versionJson) {
-
-                Log.i( "check from fir.im success! " + "\n" + versionJson);
-
-                try {
-                    JSONObject jsonObject = new JSONObject(versionJson);
-                    String name = jsonObject.getString("name");
-                    String version = jsonObject.getString("version");
-                    String changelog = jsonObject.getString("changelog");
-                    String updated_at = jsonObject.getString("updated_at");
-                    String versionShort = jsonObject.getString("versionShort");
-                    build = jsonObject.getInt("build");
-                    installUrl = jsonObject.getString("installUrl");
-                    information = "更新内容:\n" + changelog + "\n" + "版本号：" + versionShort;
-                } catch (Exception E) {
-                    E.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void onFail(Exception exception) {
-                exception.printStackTrace();
-                android.util.Log.i("fir", "check fir.im fail! " + "\n" + exception.getMessage());
-            }
-
-            @Override
-            public void onStart() {
-                // Toast.makeText(getApplicationContext(), "正在获取", Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onFinish() {
-
-                //Toast.makeText(getApplicationContext(), "检测完成", Toast.LENGTH_SHORT).show();
-                Log.i(build + "---" + version1);
-                if (build > version1) {
-                    initDialog();
-                }
-            }
-        });
-    }
-
-    /**
-     * 初始化dialog
-     */
-    public void initDialog() {
-        /**
-         * 状态选择
-         */
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("新版提示");
-        builder.setMessage(information);
-        builder.setPositiveButton("前往下载", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(installUrl));
-                startActivity(intent);
-                finish();
-            }
-        });
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
-        builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
-            @Override
-            public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-                    return true;
-                }
-                return false;
-            }
-        });
-        builder.show();
-    }
-
-    /**
-     * 获取版本号
-     *
-     * @return 当前应用的版本号
-     */
-    public String getVersion() {
-        String verson_name = "";
-        try {
-            PackageManager manager = this.getPackageManager();
-            PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
-            verson_name = info.versionName;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return verson_name;
     }
 
 }
