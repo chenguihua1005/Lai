1cc5ffe306d82236a6ebcaa51d6cbccd21d38eb9
guan <jerry.guan@softtek.com>
2016-11-22 17:01:46 +0800

    jerry

--- app/src/main/java/com/softtek/lai/module/bodygame3/more/view/InvitationListActivity.java
+++ app/src/main/java/com/softtek/lai/module/bodygame3/more/view/InvitationListActivity.java
@@ -1,8 +1,7 @@
 package com.softtek.lai.module.bodygame3.more.view;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ExpandableListView;
@@ -17,7 +16,7 @@ import com.softtek.lai.R;
 import com.softtek.lai.common.BaseActivity;
 import com.softtek.lai.common.ResponseData;
 import com.softtek.lai.common.UserInfoModel;
-import com.softtek.lai.module.bodygame3.more.adapter.MyExpandableAdapter;
+import com.softtek.lai.module.bodygame3.more.adapter.InvitatedExpandableAdapter;
 import com.softtek.lai.module.bodygame3.more.model.ClassModel;
 import com.softtek.lai.module.bodygame3.more.model.InvitatedContact;
 import com.softtek.lai.module.bodygame3.more.net.MoreService;
@@ -29,6 +28,7 @@ import java.util.List;
 import java.util.Map;
 
 import butterknife.InjectView;
+import retrofit.RetrofitError;
 import retrofit.client.Response;
 import zilla.libcore.api.ZillaApi;
 import zilla.libcore.ui.InjectLayout;
@@ -50,10 +50,9 @@ public class InvitationListActivity extends BaseActivity implements View.OnClick
 
     @InjectView(R.id.lv)
     PullToRefreshExpandableListView lv;
-    MyExpandableAdapter adapter;
+    InvitatedExpandableAdapter adapter;
     private ClassModel model;
-    Map<String,List<String>> datas=new HashMap<>();
-    private List<String> groups=new ArrayList<>();
+
     private int pageIndex;
     @Override
     protected void initViews() {
@@ -62,16 +61,14 @@ public class InvitationListActivity extends BaseActivity implements View.OnClick
         ll_left.setOnClickListener(this);
         fl_right.setOnClickListener(this);
         lv.setOnRefreshListener(this);
-        lv.setMode(PullToRefreshBase.Mode.BOTH);
+        lv.setMode(PullToRefreshBase.Mode.PULL_FROM_END);
     }
 
     @Override
     protected void initDatas() {
-        //adapter=new MyExpandableAdapter(this,datas,parentList);
+        adapter=new InvitatedExpandableAdapter(this,datas,groups);
         lv.getRefreshableView().setAdapter(adapter);
-//        for (int i = 0; i < parentList.length; i++) {
-//            lv.getRefreshableView().expandGroup(i);
-//        }
+
         lv.getRefreshableView().setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
 
             @Override
@@ -82,21 +79,76 @@ public class InvitationListActivity extends BaseActivity implements View.OnClick
         });
         Bundle bundle=getIntent().getBundleExtra("class");
         model=bundle.getParcelable("class");
-        new Handler(Looper.myLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                lv.setRefreshing();
-            }
-        },300);
+        pageIndex=1;
+        dialogShow("加载中...");
+        ZillaApi.NormalRestAdapter.create(MoreService.class)
+                .getInvitatedContactList(
+                        UserInfoModel.getInstance().getToken(),
+                        UserInfoModel.getInstance().getUserId(),
+                        model.getClassId(),
+                        20, pageIndex,
+                        new RequestCallback<ResponseData<List<InvitatedContact>>>() {
+                            @Override
+                            public void success(ResponseData<List<InvitatedContact>> data, Response response) {
+                                dialogDissmiss();
+                                lv.onRefreshComplete();
+                                if(data.getStatus()==200){
+                                    onResult(data.getData());
+                                }
+                            }
+
+                            @Override
+                            public void failure(RetrofitError error) {
+                                super.failure(error);
+                                dialogDissmiss();
+                            }
+                        });
 
     }
-
-    private void onResult(List<InvitatedContact> models,boolean isRefresh){
-        if(isRefresh){
-            groups.clear();
-            datas.clear();
+    Map<String,List<InvitatedContact>> datas=new HashMap<>();
+    private List<String> groups=new ArrayList<>();
+    private void onResult(List<InvitatedContact> models){
+//        InvitatedContact aa=new InvitatedContact();
+//        aa.setJoinGroupName("aa");
+//        aa.setInviterUserName("关谷奥匈");
+//        models.add(aa);
+//        InvitatedContact gg=new InvitatedContact();
+//        gg.setJoinGroupName("gg");
+//        gg.setInviterUserName("dsaa");
+//        models.add(gg);
+//        InvitatedContact bb=new InvitatedContact();
+//        bb.setJoinGroupName("bb");
+//        bb.setInviterUserName("asda");
+//        models.add(bb);
+//        InvitatedContact cc=new InvitatedContact();
+//        cc.setJoinGroupName("cc");
+//        cc.setInviterUserName("rffds");
+//        models.add(cc);
+//        InvitatedContact dd=new InvitatedContact();
+//        dd.setJoinGroupName("cc");
+//        dd.setInviterUserName("gfd");
+//        models.add(dd);
+//        InvitatedContact ee=new InvitatedContact();
+//        ee.setJoinGroupName("dd");
+//        ee.setInviterUserName("aa");
+//        models.add(ee);
+//        InvitatedContact ff=new InvitatedContact();
+//        ff.setJoinGroupName("aa");
+//        ff.setInviterUserName("dad");
+//        models.add(ff);
+        for (InvitatedContact contact:models){
+            String groupName=contact.getJoinGroupName();
+            if(!groups.contains(groupName)){
+                groups.add(groupName);
+                List<InvitatedContact> invitatedContacts=new ArrayList<>();
+                datas.put(groupName,invitatedContacts);
+            }
+            datas.get(groupName).add(contact);
         }
-
+        adapter.notifyDataSetChanged();
+        for (int i = 0; i < groups.size(); i++) {
+            lv.getRefreshableView().expandGroup(i);
+        }
     }
 
     @Override
@@ -108,6 +160,7 @@ public class InvitationListActivity extends BaseActivity implements View.OnClick
                 break;
             case R.id.fl_right:{
                 //跳转邀请小伙伴
+                startActivity(new Intent(this,ContactsActivity.class));
             }
                 break;
         }
@@ -115,22 +168,7 @@ public class InvitationListActivity extends BaseActivity implements View.OnClick
 
     @Override
     public void onPullDownToRefresh(PullToRefreshBase<ExpandableListView> refreshView) {
-        pageIndex=1;
-        ZillaApi.NormalRestAdapter.create(MoreService.class)
-                .getInvitatedContactList(
-                        UserInfoModel.getInstance().getToken(),
-                        UserInfoModel.getInstance().getUserId(),
-                        model.getClassId(),
-                        20, pageIndex,
-                        new RequestCallback<ResponseData<List<InvitatedContact>>>() {
-                            @Override
-                            public void success(ResponseData<List<InvitatedContact>> data, Response response) {
-                                lv.onRefreshComplete();
-                                if(data.getStatus()==200){
-                                   // onResult(data.getData());
-                                }
-                            }
-                        });
+
     }
 
     @Override
@@ -147,7 +185,7 @@ public class InvitationListActivity extends BaseActivity implements View.OnClick
                             public void success(ResponseData<List<InvitatedContact>> data, Response response) {
                                 lv.onRefreshComplete();
                                 if(data.getStatus()==200){
-                                    //onResult(data.getData());
+                                    onResult(data.getData());
                                 }
                             }
                         });
