7b2d52baa09121c622e1c835c8c2978c0e2883bc
lareina.qiao <lareina.qiao@sofftek.com>
2016-11-25 18:17:44 +0800

    lareina

---
+++ app/src/main/java/com/softtek/lai/module/bodygame3/head/view/MonthHonorFragment.java
@@ -0,0 +1,39 @@
+package com.softtek.lai.module.bodygame3.head.view;
+
+import android.os.Bundle;
+
+import com.softtek.lai.common.LazyBaseFragment;
+import com.softtek.lai.module.ranking.model.RankModel;
+import com.softtek.lai.module.ranking.persenter.RankManager;
+
+/**
+ * Created by lareina.qiao on 11/25/2016.
+ */
+public class MonthHonorFragment extends LazyBaseFragment implements RankManager.RankManagerCallback  {
+
+    public static MonthHonorFragment getInstance(){
+        MonthHonorFragment fragment=new MonthHonorFragment();
+        Bundle data=new Bundle();
+        fragment.setArguments(data);
+        return fragment;
+    }
+    @Override
+    protected void lazyLoad() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+
+    @Override
+    public void getResult(RankModel result) {
+
+    }
+}
