0b91991a423c9825a6de2bef11a31dac31cc0f19
lareina.qiao <lareina.qiao@sofftek.com>
2016-12-02 17:18:17 +0800

    l

--- app/src/main/java/com/softtek/lai/module/bodygame3/head/model/NewsTopFourModel.java
+++ app/src/main/java/com/softtek/lai/module/bodygame3/head/model/NewsTopFourModel.java
@@ -5,4 +5,39 @@ package com.softtek.lai.module.bodygame3.head.model;
  */
 public class NewsTopFourModel {
     private String ImgId;
+    private String ImgUrl;
+    private String ThumbnailImgUrl;
+
+    @Override
+    public String toString() {
+        return "NewsTopFourModel{" +
+                "ImgId='" + ImgId + '\'' +
+                ", ImgUrl='" + ImgUrl + '\'' +
+                ", ThumbnailImgUrl='" + ThumbnailImgUrl + '\'' +
+                '}';
+    }
+
+    public String getImgId() {
+        return ImgId;
+    }
+
+    public void setImgId(String imgId) {
+        ImgId = imgId;
+    }
+
+    public String getImgUrl() {
+        return ImgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        ImgUrl = imgUrl;
+    }
+
+    public String getThumbnailImgUrl() {
+        return ThumbnailImgUrl;
+    }
+
+    public void setThumbnailImgUrl(String thumbnailImgUrl) {
+        ThumbnailImgUrl = thumbnailImgUrl;
+    }
 }
