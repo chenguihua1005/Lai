30b52de6e25aad72269b890e190c3094593a6c92
lareina.qiao <lareina.qiao@softtek.com>
2016-12-03 10:10:55 +0800

    lareina

---
+++ app/src/main/java/com/softtek/lai/module/bodygame3/head/model/ListGroupModel.java
@@ -0,0 +1,83 @@
+package com.softtek.lai.module.bodygame3.head.model;
+
+/**
+ * Created by 87356 on 2016/12/3.
+ */
+public class ListGroupModel {
+    private String GroupId;//小组id
+    private String GroupName;//小组名
+    private String CoachId;//教练id
+    private String CoachName;//教练名
+    private String CoachIco;//教练头像
+    private String Ranking;//排名
+    private String LossPer;//减比
+
+    @Override
+    public String toString() {
+        return "ListGroupModel{" +
+                "GroupId='" + GroupId + '\'' +
+                ", GroupName='" + GroupName + '\'' +
+                ", CoachId='" + CoachId + '\'' +
+                ", CoachName='" + CoachName + '\'' +
+                ", CoachIco='" + CoachIco + '\'' +
+                ", Ranking='" + Ranking + '\'' +
+                ", LossPer='" + LossPer + '\'' +
+                '}';
+    }
+
+    public String getGroupId() {
+        return GroupId;
+    }
+
+    public void setGroupId(String groupId) {
+        GroupId = groupId;
+    }
+
+    public String getGroupName() {
+        return GroupName;
+    }
+
+    public void setGroupName(String groupName) {
+        GroupName = groupName;
+    }
+
+    public String getCoachId() {
+        return CoachId;
+    }
+
+    public void setCoachId(String coachId) {
+        CoachId = coachId;
+    }
+
+    public String getCoachName() {
+        return CoachName;
+    }
+
+    public void setCoachName(String coachName) {
+        CoachName = coachName;
+    }
+
+    public String getCoachIco() {
+        return CoachIco;
+    }
+
+    public void setCoachIco(String coachIco) {
+        CoachIco = coachIco;
+    }
+
+    public String getRanking() {
+        return Ranking;
+    }
+
+    public void setRanking(String ranking) {
+        Ranking = ranking;
+    }
+
+    public String getLossPer() {
+        return LossPer;
+    }
+
+    public void setLossPer(String lossPer) {
+        LossPer = lossPer;
+    }
+}
---
+++ app/src/main/java/com/softtek/lai/module/bodygame3/head/model/ListTopModel.java
@@ -0,0 +1,63 @@
+package com.softtek.lai.module.bodygame3.head.model;
+
+/**
+ * Created by 87356 on 2016/12/3.
+ */
+public class ListTopModel {
+    private String UserId;//用户id
+    private String UserName;//用户名
+    private String UserIconUrl;//用户头像
+    private String Ranking;//排名
+    private String LossPer;//减比
+
+    @Override
+    public String toString() {
+        return "ListTopModel{" +
+                "UserId='" + UserId + '\'' +
+                ", UserName='" + UserName + '\'' +
+                ", UserIconUrl='" + UserIconUrl + '\'' +
+                ", Ranking='" + Ranking + '\'' +
+                ", LossPer='" + LossPer + '\'' +
+                '}';
+    }
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserIconUrl() {
+        return UserIconUrl;
+    }
+
+    public void setUserIconUrl(String userIconUrl) {
+        UserIconUrl = userIconUrl;
+    }
+
+    public String getRanking() {
+        return Ranking;
+    }
+
+    public void setRanking(String ranking) {
+        Ranking = ranking;
+    }
+
+    public String getLossPer() {
+        return LossPer;
+    }
+
+    public void setLossPer(String lossPer) {
+        LossPer = lossPer;
+    }
+}
