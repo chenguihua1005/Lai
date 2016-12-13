19adb71fbd4ec1c3c97bbbf4b2941713b2367e8c
lareina.qiao <lareina.qiao@softtek.com>
2016-12-13 15:05:01 +0800

    lareina

--- app/src/main/java/com/softtek/lai/module/bodygame3/head/net/HeadService.java
+++ app/src/main/java/com/softtek/lai/module/bodygame3/head/net/HeadService.java
@@ -7,7 +7,6 @@ import com.softtek.lai.module.bodygame3.head.model.ClassDetailModel;
 import com.softtek.lai.module.bodygame3.head.model.ClassinfoModel;
 import com.softtek.lai.module.bodygame3.head.model.ClasslistModel;
 import com.softtek.lai.module.bodygame3.head.model.HeadModel2;
-import com.softtek.lai.module.bodygame3.head.model.HonorGroupRankModel;
 import com.softtek.lai.module.bodygame3.head.model.HonorRankModel;
 import com.softtek.lai.module.bodygame3.head.model.MemberInfoModel;
 import com.softtek.lai.module.bodygame3.head.model.NewsModel;
