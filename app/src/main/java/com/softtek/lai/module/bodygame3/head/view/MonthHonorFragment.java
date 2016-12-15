ffc805e1687953d40566db7bf30c3828f33b35d4
curry.zhang <curry.zhang@softtek.com>
2016-12-15 13:54:57 +0800

    curry

--- app/src/main/java/com/softtek/lai/module/bodygame3/head/view/MonthHonorFragment.java
+++ app/src/main/java/com/softtek/lai/module/bodygame3/head/view/MonthHonorFragment.java
@@ -101,7 +101,7 @@ public class MonthHonorFragment extends LazyBaseFragment implements WeekHonorMan
     @Override
     protected void initViews() {
         Bundle bundle = getArguments();
-//        ClassId = bundle.getString("classId");
+        ClassId = bundle.getString("classId");
         selectWeight();
         newAdapter();
         ListView refreshableView = listHonorrank.getRefreshableView();
