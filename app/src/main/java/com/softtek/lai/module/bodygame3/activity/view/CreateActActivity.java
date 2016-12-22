e31f3519172e4a93d37233a64e52397fd5633c35
unknown <shelly.xu@STKWX32448.softtek.com>
2016-12-22 19:38:51 +0800

    shelly

--- app/src/main/java/com/softtek/lai/module/bodygame3/activity/view/CreateActActivity.java
+++ app/src/main/java/com/softtek/lai/module/bodygame3/activity/view/CreateActActivity.java
@@ -115,8 +115,8 @@ public class CreateActActivity extends BaseActivity implements View.OnClickListe
             adapter = new EasyAdapter<ActtypeModel>(this, acttypeModels, R.layout.gird_item) {
                 @Override
                 public void convert(ViewHolder holder, ActtypeModel data, int position) {
-                    TextView text = holder.getView(R.id.text);
-                    text.setText(data.getActivityTypeName());
+//                    TextView text = holder.getView(R.id.text);
+//                    text.setText(data.getActivityTypeName());
                     CircleImageView image = holder.getView(R.id.head_image);
                     String path = AddressManager.get("photoHost");
                     if (StringUtils.isNotEmpty(data.getActivityTypeIcon())) {
