36b39c05a746afad2a4a416933be9d5e4dfdf77a
curry.zhang <curry.zhang@softtek.com>
2016-12-26 17:17:43 +0800

    。

--- app/src/main/java/com/softtek/lai/module/bodygame3/head/view/TotalHonorFragment.java
+++ app/src/main/java/com/softtek/lai/module/bodygame3/head/view/TotalHonorFragment.java
@@ -3,7 +3,6 @@ package com.softtek.lai.module.bodygame3.head.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
