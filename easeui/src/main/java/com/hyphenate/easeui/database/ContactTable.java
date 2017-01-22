ab5ba13c986f536a9b50a3cadb21ed9b2c021c73
unknown <jessica.zhang@STKWX32677.softtek.com>
2017-01-22 16:23:16 +0800

    fix jessica

---
+++ easeui/src/main/java/com/hyphenate/easeui/database/ContactTable.java
@@ -0,0 +1,23 @@
+package com.hyphenate.easeui.database;
+
+/**
+ * Created by jessica.zhang on 2016/11/29.
+ */
+
+public class ContactTable {
+    public static final String TABLE_NAME = "ContactTable";
+    public static final String _ID = "_id";
+    public static final String Mobile = "Mobile";
+    public static final String UserName = "UserName";
+    public static final String UserEn = "UserEn";
+    public static final String Gender = "Gender";
+    public static final String Photo = "Photo";
+    public static final String UserRole = "UserRole";
+    public static final String HXAccountId = "HXAccountId";
+    public static final String AccpetTime = "AccpetTime";
+
+    public static final String Certification = "Certification";//
+    public static final String AccountId = "AccountId";//好友账号
+    public static final String AFriendId = "AFriendId";//好友关系id
+
+}
