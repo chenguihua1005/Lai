2b68f901a1e1f1cf634cf49653f1c801470a888b
lareina.qiao <lareina.qiao@softtek.com>
2017-02-24 15:00:40 +0800

    lareina

---
+++ app/src/main/java/com/softtek/lai/module/bodygame3/more/model/UserListModel.java
@@ -0,0 +1,73 @@
+package com.softtek.lai.module.bodygame3.more.model;
+
+/**
+ * Created by lareina.qiao on 2/24/2017.
+ */
+public class UserListModel {
+    private String accountId;
+    private String photoUrl;
+    private String username;
+    private String phoneNo;
+    private String gender;
+    private int roleId;
+    private int isFriend;
+
+    public int getIsFriend() {
+        return isFriend;
+    }
+
+    public void setIsFriend(int isFriend) {
+        this.isFriend = isFriend;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+
+}
