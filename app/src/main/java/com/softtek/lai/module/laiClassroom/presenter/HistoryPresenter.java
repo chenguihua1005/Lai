c4fb535c49c1d83853775138ec7003c75b8d191f
shelly.xu <shelly.xu@softtek.com>
2017-03-11 20:25:49 +0800

    shelly 历史页面完成

---
+++ app/src/main/java/com/softtek/lai/module/laiClassroom/presenter/HistoryPresenter.java
@@ -0,0 +1,51 @@
+package com.softtek.lai.module.laiClassroom.presenter;
+
+import com.softtek.lai.common.ResponseData;
+import com.softtek.lai.common.mvp.BasePersent;
+import com.softtek.lai.common.mvp.BaseView;
+import com.softtek.lai.module.laiClassroom.model.CollectModel;
+import com.softtek.lai.module.laiClassroom.net.CollectService;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import zilla.libcore.api.ZillaApi;
+
+/**
+ * Created by Lenovo-G400 on 2017/3/11.
+ */
+
+public class HistoryPresenter extends BasePersent<HistoryPresenter.getHistorydata>{
+    CollectService service;
+    CollectModel collectModel;
+
+    public HistoryPresenter(getHistorydata baseView) {
+        super(baseView);
+    }
+    public void getVisitHistory(String token,long userid,int pageindex,int pagesize){
+        service= ZillaApi.NormalRestAdapter.create(CollectService.class);
+        service.getVisitArticleHistory(token, userid, pageindex, pagesize, new Callback<ResponseData<CollectModel>>() {
+            @Override
+            public void success(ResponseData<CollectModel> Data, Response response) {
+                int status=Data.getStatus();
+                if(200==status){
+                    if(Data.getData()!=null){
+                        collectModel=Data.getData();
+                        if(getView()!=null){
+                            getView().gethistorydata(collectModel);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+    }
+
+    public interface getHistorydata extends BaseView{
+          void gethistorydata(CollectModel collectModel);
+    }
+}
