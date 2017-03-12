53b736f504611157bcd71155a33bea7dfc051683
lareina.qiao <lareina.qiao@softtek.com>
2017-03-13 02:14:07 +0800

    test

--- app/src/main/java/com/softtek/lai/module/laiClassroom/SubjectFragment.java
+++ app/src/main/java/com/softtek/lai/module/laiClassroom/SubjectFragment.java
@@ -1,9 +1,14 @@
 package com.softtek.lai.module.laiClassroom;
 
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.ggx.widgets.adapter.EasyAdapter;
@@ -11,6 +16,7 @@ import com.ggx.widgets.adapter.ViewHolder;
 import com.handmark.pulltorefresh.library.ILoadingLayout;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshGridView;
+import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 import com.softtek.lai.R;
 import com.softtek.lai.common.LazyBaseFragment;
 import com.softtek.lai.common.ResponseData;
@@ -18,6 +24,7 @@ import com.softtek.lai.common.UserInfoModel;
 import com.softtek.lai.module.laiClassroom.model.MonographicListModel;
 import com.softtek.lai.module.laiClassroom.model.TopicListModel;
 import com.softtek.lai.module.laiClassroom.net.LaiClassroomService;
+import com.softtek.lai.widgets.HorizontalListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,15 +38,22 @@ import zilla.libcore.ui.InjectLayout;
 
 //专题页面
 @InjectLayout(R.layout.fragment_subject)
-public class SubjectFragment extends LazyBaseFragment implements PullToRefreshBase.OnRefreshListener2<GridView>{
+public class SubjectFragment extends LazyBaseFragment implements PullToRefreshBase.OnRefreshListener2<ScrollView>{
 
-    @InjectView(R.id.ptrgv)
-    PullToRefreshGridView ptrgv;
+    @InjectView(R.id.prsro)
+    PullToRefreshScrollView prsro;
     @InjectView(R.id.im_nomessage)
     ImageView im_nomessage;
+    @InjectView(R.id.hor_list)
+    HorizontalListView hor_list;
+    @InjectView(R.id.grid)
+            GridView grid;
+    @InjectView(R.id.layout)
+    LinearLayout layout;
     EasyAdapter<TopicListModel>adapter;
     List<TopicListModel>topicListModels= new ArrayList<>();
     int pageindex=1;
+//    RelativeLayout re_photowall;
 
     public SubjectFragment() {
         // Required empty public constructor
@@ -48,26 +62,40 @@ public class SubjectFragment extends LazyBaseFragment implements PullToRefreshBa
 
     @Override
     protected void lazyLoad() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
 
+            @Override
+            public void run() {
+                prsro.setRefreshing();
+            }
 
+        }, 300);
 
     }
 
     @Override
     protected void initViews() {
-        ptrgv.setOnRefreshListener(this);
+        hor_list.setFocusable(true);
+        hor_list.setFocusableInTouchMode(true);
+        hor_list.requestFocus();
+        prsro.setOnRefreshListener(this);
 
-//        ptrgv.setEmptyView(im_nomessage);
-        ptrgv.setMode(PullToRefreshBase.Mode.BOTH);
-        ILoadingLayout startLabelse = ptrgv.getLoadingLayoutProxy(true, false);
+//        prsro.setEmptyView(im_nomessage);
+        prsro.setMode(PullToRefreshBase.Mode.BOTH);
+        ILoadingLayout startLabelse = prsro.getLoadingLayoutProxy(true, false);
         startLabelse.setPullLabel("下拉刷新");// 刚下拉时，显示的提示
         startLabelse.setRefreshingLabel("正在刷新数据");// 刷新时
         startLabelse.setReleaseLabel("松开立即刷新");// 下来达到一定距离时，显示的提示
-        ILoadingLayout endLabelsr = ptrgv.getLoadingLayoutProxy(false, true);
+        ILoadingLayout endLabelsr = prsro.getLoadingLayoutProxy(false, true);
         endLabelsr.setPullLabel("上拉加载更多");// 刚下拉时，显示的提示
         endLabelsr.setRefreshingLabel("正在刷新数据");
         endLabelsr.setReleaseLabel("松开立即刷新");// 下来达到一定距离时，显示的提示
 //        View headView = View.inflate(getContext(), R.layout.bodygame3_head, null);
+//        re_photowall = (RelativeLayout) headView.findViewById(R.id.re_photowall);
+//        ptrgv.getRefreshableView().
+//        ptrgv.getRefreshableView().addView(headView);
+//
+////        ptrgv.getRefreshableView().addView(headView);
         adapter=new EasyAdapter<TopicListModel>(getContext(),topicListModels,R.layout.frag_girdlist_subject_item) {
             @Override
             public void convert(ViewHolder holder, TopicListModel data, int position) {
@@ -75,7 +103,9 @@ public class SubjectFragment extends LazyBaseFragment implements PullToRefreshBa
                 tv_clickhot.setText(data.getClicks());
             }
         };
-        ptrgv.setAdapter(adapter);
+        hor_list.setAdapter(adapter);
+        grid.setAdapter(adapter);
+//        ptrgv.setAdapter(adapter);
 
     }
 
@@ -86,8 +116,18 @@ public class SubjectFragment extends LazyBaseFragment implements PullToRefreshBa
 
     }
 
+//    @Override
+//    public void onPullDownToRefresh(PullToRefreshBase<GridView> refreshView) {
+//
+//    }
+//
+//    @Override
+//    public void onPullUpToRefresh(PullToRefreshBase<GridView> refreshView) {
+//
+//    }
+
     @Override
-    public void onPullDownToRefresh(PullToRefreshBase<GridView> refreshView) {
+    public void onPullDownToRefresh(PullToRefreshBase<ScrollView> refreshView) {
         LaiClassroomService service= ZillaApi.NormalRestAdapter.create(LaiClassroomService.class);
         service.doGetArticleTopic(UserInfoModel.getInstance().getToken(), 1, 10, new Callback<ResponseData<MonographicListModel>>() {
             @Override
@@ -98,7 +138,7 @@ public class SubjectFragment extends LazyBaseFragment implements PullToRefreshBa
                     case 200:
                         topicListModels.addAll(monographicListModelResponseData.getData().getArticleTopicList());
                         adapter.notifyDataSetChanged();
-                        ptrgv.onRefreshComplete();
+                        prsro.onRefreshComplete();
                         break;
                     default:
                         break;
@@ -113,7 +153,7 @@ public class SubjectFragment extends LazyBaseFragment implements PullToRefreshBa
     }
 
     @Override
-    public void onPullUpToRefresh(PullToRefreshBase<GridView> refreshView) {
+    public void onPullUpToRefresh(PullToRefreshBase<ScrollView> refreshView) {
 
     }
 }
