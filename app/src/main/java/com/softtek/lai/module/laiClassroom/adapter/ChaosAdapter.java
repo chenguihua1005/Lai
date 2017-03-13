e45d491978b9c545b023e5a2bee9ee7834277c03
makiserikka <jia.lu@softtek.com>
2017-03-13 11:01:21 +0800

    makiserikka

--- app/src/main/java/com/softtek/lai/module/laiClassroom/adapter/ChaosAdapter.java
+++ app/src/main/java/com/softtek/lai/module/laiClassroom/adapter/ChaosAdapter.java
@@ -7,10 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.softtek.lai.R;
-import com.softtek.lai.module.bodygame3.history.adapter.RecyclerViewInfoAdapter;
 import com.softtek.lai.module.laiClassroom.model.SearchModel;
-
-import java.util.List;
 
 
 public class ChaosAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
@@ -18,10 +15,10 @@ public class ChaosAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     private static final int VIDEO = 1;
     private static final int ONE_PIC = 2;
     private static final int THREE_PIC = 3;
-    private List<SearchModel> myItems;
+    private SearchModel myItems;
     private ItemListener myListener;
 
-    public ChaosAdapter(List<SearchModel> items, ItemListener listener) {
+    public ChaosAdapter(SearchModel items, ItemListener listener) {
         myItems = items;
         myListener = listener;
     }
@@ -31,33 +28,33 @@ public class ChaosAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == VIDEO) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_video, parent, false);
-            return new ViewHolder(view);
+            return new VideoViewHolder(view);
         } else if (viewType == ONE_PIC) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_one_pic, parent, false);
-            return new ViewHolder(view);
+            return new OnePicHolder(view);
         } else if (viewType == THREE_PIC) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_three_pic, parent, false);
-            return new ViewHolder(view);
+            return new ThreePicHolder(view);
         }
         return null;
     }
 
     @Override
     public int getItemViewType(int position) {
-        int result = 0;
-        int itemType = myItems.get(position).getType();
-        int isMultiPic = myItems.get(position).getIsMultiPic();
-        if (itemType == 1){
-            if (isMultiPic == 0){
-                result = 2;
-            }else {
-                result = 3;
+        int result = 0;//默认没有
+        int itemType = myItems.getArticleList().get(position).getMediaType();
+        int isMultiPic = myItems.getArticleList().get(position).getIsMultiPic();
+        if (itemType == 1) {
+            if (isMultiPic == 0) {
+                result = 2;//1图
+            } else {
+                result = 3;//多图
             }
-        }else if (itemType == 2){
-            result = 1;
+        } else if (itemType == 2) {
+            result = 1;//视频
         }
 
         return result;
@@ -65,30 +62,40 @@ public class ChaosAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     @Override
     public int getItemCount() {
-        return myItems.size();
+        if (myItems.getArticleList() != null) {
+            return myItems.getArticleList().size();
+        }else {
+            return 0;
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ((ViewHolder) holder).setData(myItems.get(position));
+        if (getItemViewType(position) == 1) {
+            ((VideoViewHolder) holder).setData(myItems, position);
+        } else if (getItemViewType(position) == 2) {
+            ((OnePicHolder) holder).setData(myItems, position);
+        } else if (getItemViewType(position) == 3) {
+            ((ThreePicHolder) holder).setData(myItems, position);
+        }
     }
 
     public interface ItemListener {
-        void onItemClick(SearchModel item,int position);
+        void onItemClick(SearchModel item, int position);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class VideoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         // TODO - Your view members
         public SearchModel item;
 
-        public ViewHolder(View itemView) {
+        public VideoViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
             // TODO instantiate/assign view members
         }
 
-        public void setData(SearchModel item) {
+        public void setData(SearchModel item, int position) {
             this.item = item;
             // TODO set data to view
         }
@@ -96,8 +103,34 @@ public class ChaosAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
         @Override
         public void onClick(View v) {
             if (myListener != null) {
-                myListener.onItemClick(item,getAdapterPosition());
+                myListener.onItemClick(item, getAdapterPosition());
             }
+        }
+    }
+
+    public class OnePicHolder extends RecyclerView.ViewHolder {
+        public SearchModel item;
+
+        public OnePicHolder(View view) {
+            super(view);
+        }
+
+        public void setData(SearchModel item, int position) {
+            this.item = item;
+            // TODO set data to view
+        }
+    }
+
+    public class ThreePicHolder extends RecyclerView.ViewHolder {
+        public SearchModel item;
+
+        public ThreePicHolder(View view) {
+            super(view);
+        }
+
+        public void setData(SearchModel item, int position) {
+            this.item = item;
+            // TODO set data to view
         }
     }
 
---
+++ app/src/main/java/com/softtek/lai/module/laiClassroom/net/SearchService.java
@@ -0,0 +1,24 @@
+package com.softtek.lai.module.laiClassroom.net;
+
+import com.softtek.lai.common.ResponseData;
+import com.softtek.lai.module.laiClassroom.model.SearchModel;
+import com.softtek.lai.utils.RequestCallback;
+
+import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+/**
+ * Created by jia.lu on 2017/3/10.
+ */
+
+public interface SearchService {
+
+    @GET("/v1/LaiClassRoom/SearchArticle")
+    void getChaosInfo(@Header("token") String token,
+                      @Query("keyword")String keyword,
+                      @Query("pageindex")int pageIndex,
+                      @Query("pagesize")int pageSize,
+                      RequestCallback<ResponseData<SearchModel>> callback);
+}
