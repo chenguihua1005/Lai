02ed59457f3178baf55efadf247b70e0ff1d09f0
unknown <shelly.xu@STKWX32448.softtek.com>
2017-03-13 14:32:24 +0800

    shelly 创建文章详情页面

---
+++ app/src/main/java/com/softtek/lai/module/laiClassroom/ArticdetailActivity.java
@@ -0,0 +1,24 @@
+package com.softtek.lai.module.laiClassroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softtek.lai.R;
+import com.softtek.lai.common.BaseActivity;
+
+import zilla.libcore.ui.InjectLayout;
+
+@InjectLayout(R.layout.activity_articdetail)
+public class ArticdetailActivity extends BaseActivity {
+
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
