068009798b7d9e56c6b938b1e34f050ea47ba21f
lareina.qiao <lareina.qiao@softtek.com>
2017-03-15 15:58:19 +0800

    lareina

--- app/src/main/java/com/softtek/lai/module/laiClassroom/SubjectFragment.java
+++ app/src/main/java/com/softtek/lai/module/laiClassroom/SubjectFragment.java
@@ -3,13 +3,18 @@ package com.softtek.lai.module.laiClassroom;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.ggx.widgets.adapter.EasyAdapter;
-import com.ggx.widgets.adapter.ViewHolder;
 import com.google.gson.Gson;
 import com.handmark.pulltorefresh.library.ILoadingLayout;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
@@ -18,14 +23,19 @@ import com.softtek.lai.R;
 import com.softtek.lai.common.LazyBaseFragment;
 import com.softtek.lai.module.laiClassroom.adapter.SubjectAdapter;
 import com.softtek.lai.module.laiClassroom.model.ArticleTopicModel;
+import com.softtek.lai.module.laiClassroom.model.RecommendModel;
 import com.softtek.lai.module.laiClassroom.model.SubjectModel;
 import com.softtek.lai.module.laiClassroom.model.TopicModel;
 import com.softtek.lai.module.laiClassroom.presenter.SubjectPresenter;
+import com.softtek.lai.widgets.MyRelative;
+import com.softtek.lai.widgets.RoundImageView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.InjectView;
+import zilla.libcore.file.AddressManager;
 import zilla.libcore.ui.InjectLayout;
 
 //专题页面
@@ -40,10 +50,14 @@ public class SubjectFragment extends LazyBaseFragment<SubjectPresenter> implemen
     EasyAdapter<ArticleTopicModel>adapter;
     List<ArticleTopicModel> topicListModels= new ArrayList<>();
     List<TopicModel> topicModels=new ArrayList<>();
+    List<RecommendModel>recommendModels=new ArrayList<>();
     private List<String> advList = new ArrayList<>();
 
     int pageindex=1;
     SubjectAdapter subjectAdapter;
+    ViewPager viewPager;
+    MyRelative rl_container;
+
 
     public SubjectFragment() {
         // Required empty public constructor
@@ -80,7 +94,10 @@ public class SubjectFragment extends LazyBaseFragment<SubjectPresenter> implemen
         endLabelsr.setPullLabel("上拉加载更多");// 刚下拉时，显示的提示
         endLabelsr.setRefreshingLabel("正在刷新数据");
         endLabelsr.setReleaseLabel("松开立即刷新");// 下来达到一定距离时，显示的提示
-
+        View headView = View.inflate(getContext(), R.layout.expandlist_subject_group, null);
+        viewPager = (ViewPager) headView.findViewById(R.id.viewpager);
+        rl_container = (MyRelative) headView.findViewById(R.id.rl_container);
+        ple_list.getRefreshableView().addHeaderView(headView);
         subjectAdapter=new SubjectAdapter(getContext(),topicModels);
 
 //        adapter=new EasyAdapter<ArticleTopicModel>(getContext(),topicListModels,R.layout.frag_girdlist_subject_item) {
@@ -105,13 +122,16 @@ public class SubjectFragment extends LazyBaseFragment<SubjectPresenter> implemen
     @Override
     public void getSubjectart(SubjectModel subjectModel) {
         topicListModels.addAll(subjectModel.getArticleTopicList());
+        ArticleTopicModel articleTopicModel=new ArticleTopicModel();
+//        articleTopicModel=topicListModels.get(0);
+//        topicListModels.add(articleTopicModel);
         topicModels.clear();
         if (topicListModels.size()!=0||topicListModels!=null)
         {TopicModel topicModel;
             topicModel=new TopicModel();
             if (topicListModels.size()%2==0)
             {
-                for (int i=0;i<=topicListModels.size()/2;i=i+2)
+                for (int i=0;i<topicListModels.size();i=i+2)
                 {
                     List<String>topname=new ArrayList<>();
                     List<String>topimage=new ArrayList<>();
@@ -134,24 +154,8 @@ public class SubjectFragment extends LazyBaseFragment<SubjectPresenter> implemen
             }
             else {
                 for (int i=1;i<=topicListModels.size();i=i+2) {
-                    if (i==topicListModels.size())
-                    {
-
-                        List<String>topname=new ArrayList<>();
-                        List<String>topimage=new ArrayList<>();
-                        List<Integer>topclick=new ArrayList<>();
-                        List<String>topid=new ArrayList<>();
-                        topname.add(0,topicListModels.get(i-1).getTopicName());
-                        topimage.add(0,topicListModels.get(i-1).getTopicImg());
-                        topclick.add(0,topicListModels.get(i-1).getClicks());
-                        topid.add(0,topicListModels.get(i-1).getTopicId());
-                        topicModel.setTopicName(topname);
-                        topicModel.setClicks(topclick);
-                        topicModel.setTopicId(topid);
-                        topicModel.setTopicImg(topimage);
-                        topicModels.add(topicModel);
-                    }
-                    else {
+                    if (i!=topicListModels.size())
+                    {   topicModel=new TopicModel();
                         List<String>topname=new ArrayList<>();
                         List<String>topimage=new ArrayList<>();
                         List<Integer>topclick=new ArrayList<>();
@@ -159,7 +163,7 @@ public class SubjectFragment extends LazyBaseFragment<SubjectPresenter> implemen
                         topname.add(0,topicListModels.get(i-1).getTopicName());
                         topname.add(1,topicListModels.get(i).getTopicName());
                         topimage.add(0,topicListModels.get(i-1).getTopicImg());
-                        topimage.add(1,topicListModels.get(i).getTopicName());
+                        topimage.add(1,topicListModels.get(i).getTopicImg());
                         topclick.add(0,topicListModels.get(i-1).getClicks());
                         topclick.add(1,topicListModels.get(i).getClicks());
                         topid.add(0,topicListModels.get(i-1).getTopicId());
@@ -168,6 +172,23 @@ public class SubjectFragment extends LazyBaseFragment<SubjectPresenter> implemen
                         topicModel.setTopicImg(topimage);
                         topicModel.setTopicId(topid);
                         topicModel.setClicks(topclick);
+                        topicModels.add(topicModel);
+
+                    }
+                    else {
+                        topicModel=new TopicModel();
+                        List<String>topname=new ArrayList<>();
+                        List<String>topimage=new ArrayList<>();
+                        List<Integer>topclick=new ArrayList<>();
+                        List<String>topid=new ArrayList<>();
+                        topname.add(0,topicListModels.get(i-1).getTopicName());
+                        topimage.add(0,topicListModels.get(i-1).getTopicImg());
+                        topclick.add(0,topicListModels.get(i-1).getClicks());
+                        topid.add(0,topicListModels.get(i-1).getTopicId());
+                        topicModel.setTopicName(topname);
+                        topicModel.setClicks(topclick);
+                        topicModel.setTopicId(topid);
+                        topicModel.setTopicImg(topimage);
                         topicModels.add(topicModel);
                     }
 
@@ -179,9 +200,69 @@ public class SubjectFragment extends LazyBaseFragment<SubjectPresenter> implemen
         }
         ple_list.setAdapter(subjectAdapter);
         ple_list.onRefreshComplete();
+        if (subjectModel != null) {
+            if (!subjectModel.getRecommendTopicList().isEmpty()) {
+                recommendModels.clear();
+                viewPager.removeAllViews();
+                recommendModels.addAll(subjectModel.getRecommendTopicList());
+                recommendModels.addAll(subjectModel.getRecommendTopicList());
+                recommendModels.addAll(subjectModel.getRecommendTopicList());
+                recommendModels.addAll(subjectModel.getRecommendTopicList());
+                recommendModels.addAll(subjectModel.getRecommendTopicList());
+                adapterData();
+            }
+
+
+        }
 
     }
 
+    private PagerAdapter pageradapter;
+    private void adapterData() {
+
+        pageradapter = new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return recommendModels.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+
+                View view = LayoutInflater.from(getContext()).inflate(R.layout.list_subjectremend_item, container, false);
+                container.addView(view);
+                TextView tv_clickhot = (TextView) container.findViewById(R.id.tv_clickhot);
+                tv_clickhot.setText(recommendModels.get(position).getClicks() + "");
+                ImageView im_photo = (ImageView) container.findViewById(R.id.im_photo);
+                Picasso.with(getContext()).load(AddressManager.get("photoHost") + recommendModels.get(position).getTopicImg()).fit().error(R.drawable.default_icon_square)
+                        .placeholder(R.drawable.default_icon_square).into(im_photo);
+
+                return view;
+            }
+
+            @Override
+            public int getItemPosition(Object object) {
+                return POSITION_NONE;
+            }
+
+            @Override
+            public float getPageWidth(int position) {
+                return super.getPageWidth(position);
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        };
+
+        viewPager.setAdapter(pageradapter);
+    }
 
 
     @Override
