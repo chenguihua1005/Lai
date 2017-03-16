7bbd0d0f7c07fa2fe1f0ddaba98e69a559fb2aa8
jerry.guan <jerry.guan@softtek.com>
2017-03-16 18:06:57 +0800

    修改统计图

---
+++ app/src/main/java/com/softtek/lai/module/bodygame3/graph/presenter/GraphDimemsionPresenter.java
@@ -0,0 +1,61 @@
+package com.softtek.lai.module.bodygame3.graph.presenter;
+
+import com.softtek.lai.common.ResponseData;
+import com.softtek.lai.common.UserInfoModel;
+import com.softtek.lai.common.mvp.BasePresenter;
+import com.softtek.lai.common.mvp.BaseView;
+import com.softtek.lai.module.bodygame3.graph.model.GirthModel;
+import com.softtek.lai.module.bodygame3.graph.net.GraphService;
+import com.softtek.lai.utils.RequestCallback;
+
+import java.util.List;
+
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import zilla.libcore.api.ZillaApi;
+
+/**
+ * Created by jerry.guan on 3/16/2017.
+ */
+
+public class GraphDimemsionPresenter extends BasePresenter<GraphDimemsionPresenter.GraphGirthView>{
+
+    GraphService service;
+
+    public GraphDimemsionPresenter(GraphGirthView baseView) {
+        super(baseView);
+        service= ZillaApi.NormalRestAdapter.create(GraphService.class);
+    }
+
+    public void getGraph(long accountId,String classsId){
+            service.getClassMemberGirthChart(
+                            UserInfoModel.getInstance().getToken(),
+                            accountId,
+                            classsId,
+                                new RequestCallback<ResponseData<List<GirthModel>>>() {
+                                @Override
+                                public void success(ResponseData<List<GirthModel>> data, Response response) {
+                                    if(getView()!=null){
+                                        getView().onSuccess(data.getData());
+                                    }
+                                }
+
+                                @Override
+                                public void failure(RetrofitError error) {
+                                    if(getView()!=null){
+                                        getView().onFaile();
+                                    }
+                                    super.failure(error);
+                                }
+                            }
+                    );
+    }
+
+
+    public interface GraphGirthView extends BaseView{
+
+        void onSuccess(List<GirthModel> data);
+
+        void onFaile();
+    }
+}
