5041bd10a2c9d61a8f66838f53a0200b479e3919
lareina.qiao <lareina.qiao@softtek.com>
2017-04-14 14:44:23 +0800

    sh

--- app/src/main/java/com/softtek/lai/module/laicheng/VisithistoryActivity.java
+++ app/src/main/java/com/softtek/lai/module/laicheng/VisithistoryActivity.java
@@ -1,8 +1,5 @@
 package com.softtek.lai.module.laicheng;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.widget.SearchView;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -20,7 +17,6 @@ import com.softtek.lai.module.laicheng.adapter.HistoryAdapter;
 import com.softtek.lai.module.laicheng.model.HistoryModel;
 import com.softtek.lai.module.laicheng.presenter.HistoryVisitorPresenter;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +24,7 @@ import butterknife.InjectView;
 import zilla.libcore.ui.InjectLayout;
 
 @InjectLayout(R.layout.activity_visithistory)
-public class VisithistoryActivity extends BaseActivity<HistoryVisitorPresenter> implements View.OnClickListener, HistoryVisitorPresenter.HistoryVisitorView{
+public class VisithistoryActivity extends BaseActivity<HistoryVisitorPresenter> implements View.OnClickListener, HistoryVisitorPresenter.HistoryVisitorView {
     @InjectView(R.id.tv_title)
     TextView tv_title;
 
@@ -42,8 +38,8 @@ public class VisithistoryActivity extends BaseActivity<HistoryVisitorPresenter> 
     RelativeLayout im_nomessage;
     @InjectView(R.id.rl_search)
     RelativeLayout rl_search;
-//    @InjectView(R.id.et_input)
-//    SearchView et_input;//searchview
+    @InjectView(R.id.ll_nodata)
+    RelativeLayout ll_nodata;//searchview
     @InjectView(R.id.et_input)
     EditText et_input;
     @InjectView(R.id.imageView10)
@@ -65,8 +61,7 @@ public class VisithistoryActivity extends BaseActivity<HistoryVisitorPresenter> 
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (et_input.getText().toString().isEmpty())
-                {
+                if (et_input.getText().toString().isEmpty()) {
                     adapter.getFilter().filter(null);
                 }
             }
@@ -76,32 +71,16 @@ public class VisithistoryActivity extends BaseActivity<HistoryVisitorPresenter> 
 
             }
         });
-        //searchview
-//        et_input.setOnQueryTextListener(new QueryListener());
-//        et_input.setIconifiedByDefault(false);
-//        if (et_input != null) {
-//            try {        //--拿到字节码
-//                Class<?> argClass = et_input.getClass();
-//                //--指定某个私有属性,mSearchPlate是搜索框父布局的名字
-//                Field ownField = argClass.getDeclaredField("mSearchPlate");
-//                //--暴力反射,只有暴力反射才能拿到私有属性
-//                ownField.setAccessible(true);
-//                View mView = (View) ownField.get(et_input);
-//                //--设置背景
-//                mView.setBackgroundColor(Color.TRANSPARENT);
-//            }
-//            catch (Exception e) {
-//                e.printStackTrace();
-//            }}
+
     }
 
     @Override
     protected void initDatas() {
         setPresenter(new HistoryVisitorPresenter(this));
         getPresenter().GetData();
-        ptrlv.setEmptyView(im_nomessage);
+//        ptrlv.setEmptyView(im_nomessage);
         ptrlv.setTextFilterEnabled(true);
-        adapter = new HistoryAdapter(this, historyModelList,historyModelList,et_input);
+        adapter = new HistoryAdapter(this, historyModelList, et_input);
         ptrlv.setAdapter(adapter);
     }
 
@@ -109,7 +88,6 @@ public class VisithistoryActivity extends BaseActivity<HistoryVisitorPresenter> 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.ll_left:
-//                startActivity(new Intent(this,TestActivity.class));
                 finish();
                 break;
             case R.id.imageView10:
@@ -125,34 +103,14 @@ public class VisithistoryActivity extends BaseActivity<HistoryVisitorPresenter> 
 
     @Override
     public void getInfo(List<HistoryModel> historyModels) {
-        if (historyModels.isEmpty())
-        {
-            rl_search.setVisibility(View.GONE);
+        if (historyModels.isEmpty()) {
+            im_nomessage.setVisibility(View.VISIBLE);
+            ptrlv.setVisibility(View.GONE);
 
-        }
-        else {
-            historyModelList=historyModels;
-//            adapter.notifyDataSetChanged();
-
-        }
-    }
-    // 搜索文本监听器
-    private class QueryListener implements SearchView.OnQueryTextListener {
-        // 当内容被提交时执行
-        @Override
-        public boolean onQueryTextSubmit(String query) {
-            return true;
-        }
-
-        // 当搜索框内内容发生改变时执行
-        @Override
-        public boolean onQueryTextChange(String newText) {
-            if (TextUtils.isEmpty(newText)) {
-                adapter.getFilter().filter(null);  // 清楚ListView的过滤
-            } else {
-                adapter.getFilter().filter(newText); // 设置ListView的过滤关键词
-            }
-            return true;
+        } else {
+            historyModelList = historyModels;
+            rl_search.setVisibility(View.VISIBLE);
+            ptrlv.setEmptyView(ll_nodata);
         }
     }
 
--- app/src/main/java/com/softtek/lai/module/laicheng/net/VisitorService.java
+++ app/src/main/java/com/softtek/lai/module/laicheng/net/VisitorService.java
@@ -1,12 +1,16 @@
 package com.softtek.lai.module.laicheng.net;
 
 import com.softtek.lai.common.ResponseData;
+import com.softtek.lai.module.laicheng.model.HistoryModel;
 import com.softtek.lai.module.laicheng.model.VisitorModel;
 import com.softtek.lai.module.laicheng.model.Visitsmodel;
+
+import java.util.List;
 
 import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.Field;
+import retrofit.http.GET;
 import retrofit.http.Header;
 import retrofit.http.POST;
 
@@ -23,4 +27,10 @@ public interface VisitorService {
             @Body VisitorModel visitorModel,
             Callback<ResponseData<Visitsmodel>> callback
             );
+    //历史访客信息列表
+    @GET("/v1/DataSync/GetVisitorHistory")
+    void doGetVisitHistory(
+            @Header("token") String token,
+            Callback<ResponseData<List<HistoryModel>>>callback
+    );
 }
