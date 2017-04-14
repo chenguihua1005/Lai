251e5493fe83a082a75c385793d5e281033d3191
unknown <shelly.xu@STKWX32448.softtek.com>
2017-04-14 14:57:03 +0800

    shelly 为访客测量获取最新的测量数据

--- app/src/main/java/com/softtek/lai/module/laicheng/net/VisitorService.java
+++ app/src/main/java/com/softtek/lai/module/laicheng/net/VisitorService.java
@@ -1,14 +1,17 @@
 package com.softtek.lai.module.laicheng.net;
 
 import com.softtek.lai.common.ResponseData;
+import com.softtek.lai.module.laicheng.model.GetVisitorModel;
 import com.softtek.lai.module.laicheng.model.VisitorModel;
 import com.softtek.lai.module.laicheng.model.Visitsmodel;
 
 import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.Field;
+import retrofit.http.GET;
 import retrofit.http.Header;
 import retrofit.http.POST;
+import retrofit.http.Query;
 
 /**
  * Created by shelly.xu on 4/10/2017.
@@ -16,11 +19,17 @@ import retrofit.http.POST;
 
 public interface VisitorService {
 
-
     @POST("/v1/LaiBalanceSync/SaveLBVisitor")
     void commitvisit(
             @Header("token") String token,
             @Body VisitorModel visitorModel,
             Callback<ResponseData<Visitsmodel>> callback
             );
+
+    @GET("/v1/DataSync/GetLastRecord")
+    void getData(
+            @Header("token") String token,
+            @Query("type") int type,
+            Callback<ResponseData<GetVisitorModel>> callback
+    );
 }
