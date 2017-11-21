22604d5a0687c9053e8585e26a33f5ed35144d1c
jessica <jessica.zhang@softtek.com>
2017-11-21 15:00:16 +0800

    <fix jessica> 结构调整

--- app/src/main/java/com/softtek/lai/module/customermanagement/CustomerManageFragment.java
+++ app/src/main/java/com/softtek/lai/module/customermanagement/CustomerManageFragment.java
@@ -1,6 +1,9 @@
 package com.softtek.lai.module.customermanagement;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
@@ -8,19 +11,15 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.handmark.pulltorefresh.library.ILoadingLayout;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
-import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.softtek.lai.R;
 import com.softtek.lai.common.LazyBaseFragment;
-import com.softtek.lai.module.customermanagement.adapter.CustomerAdapter;
 import com.softtek.lai.module.customermanagement.adapter.CustomerMenuAdapter;
-import com.softtek.lai.module.customermanagement.model.CustomerModel;
+import com.softtek.lai.module.customermanagement.adapter.TypeFragmentAdapter;
 import com.softtek.lai.module.customermanagement.view.AddCustomerActivity;
-import com.softtek.lai.module.customermanagement.view.CustomerDetailActivity;
+import com.softtek.lai.module.customermanagement.view.IntendCustomerFragment;
 import com.softtek.lai.module.customermanagement.view.RegistForCustomerActivity;
 
 import java.util.ArrayList;
@@ -34,7 +33,7 @@ import zilla.libcore.ui.InjectLayout;
  */
 
 @InjectLayout(R.layout.activity_customer_manage)
-public class CustomerManageFragment extends LazyBaseFragment implements View.OnClickListener, AdapterView.OnItemClickListener, PullToRefreshBase.OnRefreshListener2<ListView> {
+public class CustomerManageFragment extends LazyBaseFragment implements View.OnClickListener {
     @InjectView(R.id.ll_left)
     LinearLayout ll_left;
     @InjectView(R.id.tv_title)
@@ -49,13 +48,15 @@ public class CustomerManageFragment extends LazyBaseFragment implements View.OnC
     GridView menu_gv;
     private CustomerMenuAdapter menuAdapter;
 
-    @InjectView(R.id.plv_audit)
-    PullToRefreshListView plv_audit;
-    @InjectView(R.id.ll_nomessage)
-    RelativeLayout im_nomessage;
 
-    private CustomerAdapter customerAdapter;
-    private List<CustomerModel> modelList = new ArrayList<CustomerModel>();
+    @InjectView(R.id.tab)
+    TabLayout tab;
+    @InjectView(R.id.container)
+    ViewPager container;
+
+    List<Fragment> fragments = new ArrayList<>();
+    TypeFragmentAdapter adapter;
+
 
 
     @Override
@@ -64,18 +65,12 @@ public class CustomerManageFragment extends LazyBaseFragment implements View.OnC
         ll_left.setVisibility(View.INVISIBLE);
         iv_email.setBackgroundResource(R.drawable.club);
 
-        plv_audit.setOnItemClickListener(this);
-        plv_audit.setMode(PullToRefreshBase.Mode.DISABLED);
-        plv_audit.setOnRefreshListener(this);
-        plv_audit.setEmptyView(im_nomessage);
-        ILoadingLayout startLabelse = plv_audit.getLoadingLayoutProxy(true, false);
-        startLabelse.setPullLabel("下拉刷新");// 刚下拉时，显示的提示
-        startLabelse.setRefreshingLabel("正在刷新数据");// 刷新时
-        startLabelse.setReleaseLabel("松开立即刷新");// 下来达到一定距离时，显示的提示
-        ILoadingLayout endLabelsr = plv_audit.getLoadingLayoutProxy(false, true);
-        endLabelsr.setPullLabel("上拉加载更多");// 刚下拉时，显示的提示
-        endLabelsr.setRefreshingLabel("正在加载数据");
-        endLabelsr.setReleaseLabel("松开立即加载");// 下来达到一定距离时，显示的提示
+        fragments.add(IntendCustomerFragment.getInstance());
+        fragments.add(IntendCustomerFragment.getInstance());
+
+        adapter = new TypeFragmentAdapter(getChildFragmentManager(), fragments);
+        container.setAdapter(adapter);
+        tab.setupWithViewPager(container);
 
 
     }
@@ -98,24 +93,11 @@ public class CustomerManageFragment extends LazyBaseFragment implements View.OnC
         });
 
 
-        modelList.add(new CustomerModel("haha", "Tom", "由张三于2017.10.10添加"));
-        modelList.add(new CustomerModel("haha", "Nicole", "由张三于2017.10.10添加"));
-        customerAdapter = new CustomerAdapter(getContext(), modelList);
-        plv_audit.setAdapter(customerAdapter);
-
-//        plv_audit.onRefreshComplete();
-
-
     }
 
     @Override
     protected void lazyLoad() {
-//        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                plv_audit.setRefreshing();
-//            }
-//        }, 300);
+
     }
 
     @Override
@@ -124,25 +106,9 @@ public class CustomerManageFragment extends LazyBaseFragment implements View.OnC
             case R.id.ll_left:
 
                 break;
-
-
         }
 
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(getContext(), CustomerDetailActivity.class);
-        startActivity(intent);
-    }
 
-    @Override
-    public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
-
-    }
-
-    @Override
-    public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
-
-    }
 }
---
+++ app/src/main/java/com/softtek/lai/module/customermanagement/view/IntendCustomerFragment.java
@@ -0,0 +1,96 @@
+package com.softtek.lai.module.customermanagement.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.handmark.pulltorefresh.library.ILoadingLayout;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.softtek.lai.R;
+import com.softtek.lai.common.LazyBaseFragment;
+import com.softtek.lai.module.customermanagement.adapter.CustomerAdapter;
+import com.softtek.lai.module.customermanagement.model.CustomerModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.InjectView;
+import zilla.libcore.ui.InjectLayout;
+
+/**
+ * Created by jessica.zhang on 11/21/2017.
+ */
+
+@InjectLayout(R.layout.fragment_customer)
+public class IntendCustomerFragment extends LazyBaseFragment implements AdapterView.OnItemClickListener, PullToRefreshBase.OnRefreshListener2<ListView> {
+    @InjectView(R.id.plv_audit)
+    PullToRefreshListView plv_audit;
+    @InjectView(R.id.ll_nomessage)
+    RelativeLayout im_nomessage;
+
+
+    private CustomerAdapter customerAdapter;
+    private List<CustomerModel> modelList = new ArrayList<CustomerModel>();
+
+    public static Fragment getInstance() {
+        Fragment fragment = new IntendCustomerFragment();
+        Bundle bundle = new Bundle();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    protected void lazyLoad() {
+
+    }
+
+    @Override
+    protected void initViews() {
+        plv_audit.setOnItemClickListener(this);
+        plv_audit.setMode(PullToRefreshBase.Mode.BOTH);
+        plv_audit.setOnRefreshListener(this);
+        plv_audit.setEmptyView(im_nomessage);
+        ILoadingLayout startLabelse = plv_audit.getLoadingLayoutProxy(true, false);
+        startLabelse.setPullLabel("下拉刷新");// 刚下拉时，显示的提示
+        startLabelse.setRefreshingLabel("正在刷新数据");// 刷新时
+        startLabelse.setReleaseLabel("松开立即刷新");// 下来达到一定距离时，显示的提示
+        ILoadingLayout endLabelsr = plv_audit.getLoadingLayoutProxy(false, true);
+        endLabelsr.setPullLabel("上拉加载更多");// 刚下拉时，显示的提示
+        endLabelsr.setRefreshingLabel("正在加载数据");
+        endLabelsr.setReleaseLabel("松开立即加载");// 下来达到一定距离时，显示的提示
+
+        modelList.add(new CustomerModel("haha", "Tom", "由张三于2017.10.10添加"));
+        modelList.add(new CustomerModel("haha", "Nicole", "由张三于2017.10.10添加"));
+        customerAdapter = new CustomerAdapter(getContext(), modelList);
+        plv_audit.setAdapter(customerAdapter);
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(getContext(), CustomerDetailActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+        plv_audit.onRefreshComplete();
+
+    }
+
+    @Override
+    public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+        plv_audit.onRefreshComplete();
+
+    }
+}
